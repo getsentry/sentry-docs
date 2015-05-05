69915d2e027279f3ba9eda380346e7a075121fe3
Armin Ronacher <armin.ronacher@active-4.com>
2015-05-06 00:49:56 +0200

    Moved design largely to webpack based

---
+++ design/webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'app': './js/main.js',
+    'styles': './less/main.less',
+    'vendor': [
+      'jquery',
+      'bootstrap'
+    ]
+  },
+  output: {
+    path: __dirname + "/gen",
+    publicPath: "/static/",
+    filename: '[name].js'
+  },
+  devtool: '#source-map',
+  resolve: {
+    modulesDirectories: ['../node_modules'],
+    extensions: ['', '.js']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.(woff|eot|ttf|svg)(\?.*)?$/,
+        loader: 'url?limit=5000'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+    new ExtractTextPlugin('styles.css', {
+      allChunks: true
+    })
+  ],
+  externals: {},
+  resolve: {
+    extensions: ['', '.js']
+  }
+}
