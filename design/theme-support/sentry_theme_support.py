2869dd07a32bb28fa78a8a8d0634089a4c22fc5b
Armin Ronacher <armin.ronacher@active-4.com>
2015-07-21 21:41:42 +0200

    Updated syntax highlighting colors to the colorscheme

--- design/theme-support/sentry_theme_support.py
+++ design/theme-support/sentry_theme_support.py
@@ -3,6 +3,22 @@ from pygments.token import Keyword, Name, Comment, String, Error, \
      Number, Operator, Generic, Whitespace, Punctuation, Other, Literal
 
 
+black = "#111111"
+trim = "#d0d8de"
+gray_dark = "#4b4f5c"
+gray = "#696D80"
+gray_light = "#B5B9BD"
+orange = "#F78300"
+sentry_orange = "#fb4226"
+blue = "#2eb0f7"
+green = "#34c08b"
+yellow = "#fece09"
+red = "#e8535a"
+pink = "#DB3B8A"
+purple = "#a47ac6"
+teal = "#45c2c9"
+
+
 class SentryStyle(Style):
     #background_color = "#f8f8f8"
     default_style = ""
@@ -12,74 +28,74 @@ class SentryStyle(Style):
         #Text:                     "", # class:  ''
         Whitespace:                "underline #f8f8f8",      # class: 'w'
         Error:                     "#a40000 border:#ef2929", # class: 'err'
-        Other:                     "#000000",                # class 'x'
+        Other:                     black,                # class 'x'
 
-        Comment:                   "italic #c10000", # class: 'c'
+        Comment:                   "italic " + red,  # class: 'c'
         Comment.Preproc:           "noitalic",       # class: 'cp'
 
-        Keyword:                   "#1791F8",   # class: 'k'
-        Keyword.Constant:          "#1791F8",   # class: 'kc'
-        Keyword.Declaration:       "#1791F8",   # class: 'kd'
-        Keyword.Namespace:         "#1791F8",   # class: 'kn'
-        Keyword.Pseudo:            "#1791F8",   # class: 'kp'
-        Keyword.Reserved:          "#1791F8",   # class: 'kr'
-        Keyword.Type:              "#1791F8",   # class: 'kt'
+        Keyword:                   blue,   # class: 'k'
+        Keyword.Constant:          blue,   # class: 'kc'
+        Keyword.Declaration:       blue,   # class: 'kd'
+        Keyword.Namespace:         blue,   # class: 'kn'
+        Keyword.Pseudo:            blue,   # class: 'kp'
+        Keyword.Reserved:          blue,   # class: 'kr'
+        Keyword.Type:              blue,   # class: 'kt'
 
-        Operator:                  "#582800",   # class: 'o'
-        Operator.Word:             "#1791F8",   # class: 'ow' - like keywords
+        Operator:                  gray_dark,   # class: 'o'
+        Operator.Word:             blue,   # class: 'ow' - like keywords
 
-        Punctuation:               "#000000",   # class: 'p'
+        Punctuation:               black,   # class: 'p'
 
         # because special names such as Name.Class, Name.Function, etc.
         # are not recognized as such later in the parsing, we choose them
         # to look the same as ordinary variables.
-        Name:                      "#000000",        # class: 'n'
-        Name.Attribute:            "#c4a000",        # class: 'na' - to be revised
-        Name.Builtin:              "#1791F8",        # class: 'nb'
-        Name.Builtin.Pseudo:       "#3465a4",        # class: 'bp'
-        Name.Class:                "#000000",        # class: 'nc' - to be revised
-        Name.Constant:             "#000000",        # class: 'no' - to be revised
-        Name.Decorator:            "#888",           # class: 'nd' - to be revised
-        Name.Entity:               "#ce5c00",        # class: 'ni'
-        Name.Exception:            "#cc0000",   # class: 'ne'
-        Name.Function:             "#000000",        # class: 'nf'
-        Name.Property:             "#000000",        # class: 'py'
-        Name.Label:                "#f57900",        # class: 'nl'
-        Name.Namespace:            "#000000",        # class: 'nn' - to be revised
-        Name.Other:                "#000000",        # class: 'nx'
-        Name.Tag:                  "#1791F8",   # class: 'nt' - like a keyword
-        Name.Variable:             "#000000",        # class: 'nv' - to be revised
-        Name.Variable.Class:       "#000000",        # class: 'vc' - to be revised
-        Name.Variable.Global:      "#000000",        # class: 'vg' - to be revised
-        Name.Variable.Instance:    "#000000",        # class: 'vi' - to be revised
+        Name:                      black,        # class: 'n'
+        Name.Attribute:            orange,        # class: 'na' - to be revised
+        Name.Builtin:              blue,        # class: 'nb'
+        Name.Builtin.Pseudo:       teal,        # class: 'bp'
+        Name.Class:                black,        # class: 'nc' - to be revised
+        Name.Constant:             black,        # class: 'no' - to be revised
+        Name.Decorator:            gray,           # class: 'nd' - to be revised
+        Name.Entity:               orange,        # class: 'ni'
+        Name.Exception:            red,   # class: 'ne'
+        Name.Function:             black,        # class: 'nf'
+        Name.Property:             black,        # class: 'py'
+        Name.Label:                sentry_orange,        # class: 'nl'
+        Name.Namespace:            black,        # class: 'nn' - to be revised
+        Name.Other:                black,        # class: 'nx'
+        Name.Tag:                  blue,   # class: 'nt' - like a keyword
+        Name.Variable:             black,        # class: 'nv' - to be revised
+        Name.Variable.Class:       black,        # class: 'vc' - to be revised
+        Name.Variable.Global:      black,        # class: 'vg' - to be revised
+        Name.Variable.Instance:    black,        # class: 'vi' - to be revised
 
-        Number:                    "#990000",        # class: 'm'
+        Number:                    teal,        # class: 'm'
 
-        Literal:                   "#000000",        # class: 'l'
-        Literal.Date:              "#000000",        # class: 'ld'
+        Literal:                   black,        # class: 'l'
+        Literal.Date:              black,        # class: 'ld'
 
-        String:                    "#c17200",        # class: 's'
-        String.Backtick:           "#c17200",        # class: 'sb'
-        String.Char:               "#c17200",        # class: 'sc'
-        String.Doc:                "italic #c10000", # class: 'sd' - like a comment
-        String.Double:             "#c17200",        # class: 's2'
-        String.Escape:             "#c17200",        # class: 'se'
-        String.Heredoc:            "#c17200",        # class: 'sh'
-        String.Interpol:           "#c17200",        # class: 'si'
-        String.Other:              "#c17200",        # class: 'sx'
-        String.Regex:              "#c17200",        # class: 'sr'
-        String.Single:             "#c17200",        # class: 's1'
-        String.Symbol:             "#c17200",        # class: 'ss'
+        String:                    green,        # class: 's'
+        String.Backtick:           green,        # class: 'sb'
+        String.Char:               green,        # class: 'sc'
+        String.Doc:                "italic " + purple, # class: 'sd' - like a comment
+        String.Double:             green,        # class: 's2'
+        String.Escape:             green,        # class: 'se'
+        String.Heredoc:            green,        # class: 'sh'
+        String.Interpol:           green,        # class: 'si'
+        String.Other:              green,        # class: 'sx'
+        String.Regex:              green,        # class: 'sr'
+        String.Single:             green,        # class: 's1'
+        String.Symbol:             green,        # class: 'ss'
 
-        Generic:                   "#000000",        # class: 'g'
-        Generic.Deleted:           "#a40000",        # class: 'gd'
-        Generic.Emph:              "italic #000000", # class: 'ge'
-        Generic.Error:             "#ef2929",        # class: 'gr'
-        Generic.Heading:           "bold #000080",   # class: 'gh'
-        Generic.Inserted:          "#00A000",        # class: 'gi'
-        Generic.Output:            "#888",           # class: 'go'
-        Generic.Prompt:            "#745334",        # class: 'gp'
-        Generic.Strong:            "bold #000000",   # class: 'gs'
-        Generic.Subheading:        "bold #800080",   # class: 'gu'
-        Generic.Traceback:         "bold #a40000",   # class: 'gt'
+        Generic:                   black,        # class: 'g'
+        Generic.Deleted:           red,        # class: 'gd'
+        Generic.Emph:              "italic " + black, # class: 'ge'
+        Generic.Error:             red,        # class: 'gr'
+        Generic.Heading:           "bold " + blue,   # class: 'gh'
+        Generic.Inserted:          green,        # class: 'gi'
+        Generic.Output:            gray,           # class: 'go'
+        Generic.Prompt:            gray_dark,        # class: 'gp'
+        Generic.Strong:            "bold " + black,   # class: 'gs'
+        Generic.Subheading:        "bold " + gray_dark,   # class: 'gu'
+        Generic.Traceback:         "bold " + red,   # class: 'gt'
     }
