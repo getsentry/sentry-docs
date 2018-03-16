25af2ccb1ab3035cb48467e312c1979b004c39df
cameronmcefee <cm@cameronmcefee.com>
2018-03-16 15:47:11 -0700

    Hook up dropdowns

--- src/_assets/js/script.js
+++ src/_assets/js/script.js
@@ -1,4 +1,6 @@
 //= require _vendor/jquery/jquery
+//= require _vendor/popper/umd/popper
 //= require _vendor/bootstrap/util
 //= require _vendor/bootstrap/tab
+//= require _vendor/bootstrap/dropdown
 //= require _includes/PlatformSpecificContent
