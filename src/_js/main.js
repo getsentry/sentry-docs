bd314f3bd84c42d018c159c632bf23e204ae4e37
cameronmcefee <cm@cameronmcefee.com>
2018-06-21 13:20:10 -0700

    Add tracking widget

--- src/_js/main.js
+++ src/_js/main.js
@@ -1,11 +1,13 @@
 import 'bootstrap';
 import PlatformContent from './lib/PlatformContent';
 import InteractiveContent from './lib/InteractiveContent';
+import Tracking from './lib/Tracking';
 import User from './lib/User';
 
 $(function() {
   PlatformContent.init();
   InteractiveContent.init();
+  Tracking.init();
   window.User = new User();
   window.User.init();
   $('[data-toggle="tooltip"]').tooltip();
