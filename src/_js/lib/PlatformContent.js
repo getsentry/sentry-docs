334889b2d10835d7454b2762fde3330737d9edfd
cameronmcefee <cm@cameronmcefee.com>
2018-06-22 11:39:23 -0700

    update platform switcher to accept a query param

--- src/_js/lib/PlatformContent.js
+++ src/_js/lib/PlatformContent.js
@@ -1,22 +1,51 @@
-const init = function() {
-  const KEY = 'preferredPlatform';
-  const showPreferredPlatform = function() {
-    const preferredPlatform = localStorage.getItem(KEY);
-    if (preferredPlatform) {
-      $('[data-platform="' + preferredPlatform + '"]').click();
-    }
-  };
+import qs from 'query-string';
 
-  const $dropdownItems = $('[data-platform-specific-content] .dropdown-item');
-  $dropdownItems.on('click', function(event) {
+const KEY = 'preferredPlatform';
+
+const showPlatform = function(slug) {
+  const platform = window.supportedPlatforms.find(p => p.slug === slug);
+  if (!platform) return;
+
+  const $contentBlocks = $('[data-platform-specific-content]');
+
+  $contentBlocks.each((i, el) => {
+    const $block = $(el);
+    // Update dropdown target title
+    $block.find('[data-toggle="dropdown"]').text(platform.name);
+
+    // Update active state of dropdown items
+    const $dropdownItems = $block.find('[data-toggle="platform"]');
+    $dropdownItems.each((i, el) => {
+      const $el = $(el);
+      const isActive = $el.attr('data-platform') === platform.slug;
+      $el.toggleClass('active', isActive);
+      $el.attr('aria-selected', isActive);
+    });
+
+    // Update the active state of the tab panes
+    const $activeItem = $dropdownItems.filter('.active');
+    const activeID = $activeItem.attr('href').replace('#', '');
+    const $tabPanes = $block.find('.tab-pane');
+    $tabPanes.each((i, el) => {
+      const $el = $(el);
+      const isActive = $el.attr('id') === activeID;
+      $el.toggleClass('show active', isActive);
+    });
+  });
+};
+
+const init = function() {
+  $(document).on('click', '[data-toggle="platform"]', function(event) {
+    event.preventDefault();
     const $target = $(event.target);
-    const $parent = $target.closest('[data-platform-specific-content]');
-    const platform = $target.data('platform');
-    localStorage.setItem(KEY, platform);
-    $parent.find('.dropdown-toggle').text(platform);
+    const slug = $target.data('platform');
+    localStorage.setItem(KEY, slug);
+    showPlatform(slug);
   });
 
-  showPreferredPlatform();
+  const queryPlatform = qs.parse(location.search).platform;
+  const preferredPlatform = localStorage.getItem(KEY);
+  showPlatform(queryPlatform || preferredPlatform);
 };
 
 export default { init };
--- src/collections/_dev_components/layout-components/platform-content.md
+++ src/collections/_dev_components/layout-components/platform-content.md
@@ -2,25 +2,35 @@
 title: Platform Content
 source: components/platform_content.html
 example_string: Hello world
+
 example_1:
   JavaScript: |
-    This is a JavaScript example.
-
     ``` javascript
     console.log("{{ page.example_string }}")
     ```
   Python: |
-    This is a Python example.
-
     ``` python
     print("{{ page.example_string }}")
     ```
   Ruby: |
-    This is a Ruby example.
-
     ``` ruby
     puts "{{ page.example_string }}"
     ```
+
+example_2:
+  JavaScript: |
+    ## I am an example about JavaScript
+
+    Did you know 0 == false but 0 !== false?
+  Python: |
+    ```
+    # This is a Python example. A python is a snake.
+    ```
+  Ruby: |
+    - In Ruby
+    - You have to
+    - Say `end` a lot.
+
 ---
 
 You can customize content by platform. The switcher will globally remember the prefer language and load it by default until changed. Content is run through the Liquid compiler, then through the Markdown compiler, making it possible to create dynamic, rich styled content.
@@ -38,20 +48,14 @@ You can customize content by platform. The switcher will globally remember the p
 example_string: Hello world
 example_content:
   JavaScript: |
-    This is a JavaScript example.
-
     ``` javascript
     console.log("{{ page.example_string }}")
     ```
   Python: |
-    This is a Python example.
-
     ``` python
     print("{{ page.example_string }}")
     ```
   Ruby: |
-    This is a Ruby example.
-
     ``` ruby
     puts "{{ page.example_string }}"
     ```
@@ -64,4 +68,34 @@ example_content:
 
 <div class="p-3 mb-3 mb-md-5 border rounded content-flush-bottom">
 {% include components/platform_content.html content=page.example_1 %}
+</div>
+
+### Arbitrary content
+
+#### Source
+
+```liquid
+{% raw %}---
+example_content:
+  JavaScript: |
+    ## I am an example about JavaScript
+
+    Did you know 0 == false but 0 !== false?
+  Python: |
+    ```
+    # This is a Python example. A python is a snake.
+    ```
+  Ruby: |
+    - In Ruby
+    - You have to
+    - Say `end` a lot.
+---
+
+{% include platform_content.html content=page.example_content %}{% endraw %}
+```
+
+#### Output
+
+<div class="p-3 mb-3 mb-md-5 border rounded content-flush-bottom">
+{% include components/platform_content.html content=page.example_2 %}
 </div>
