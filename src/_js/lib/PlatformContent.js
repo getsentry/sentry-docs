a2c5912cc4b02fa772e2d210035c8f180d4e125f
cameronmcefee <cm@cameronmcefee.com>
2018-06-22 13:06:20 -0700

    add platform token to all docs links when one is supplied

--- src/_js/lib/PlatformContent.js
+++ src/_js/lib/PlatformContent.js
@@ -2,12 +2,27 @@ import qs from 'query-string';
 
 const KEY = 'preferredPlatform';
 
+// Get the object associated with a given platform slug.
+//
+//  slug - slug matching a platform in data/platforms.yml
+//
+// Returns an object or null if no platform matches the slug.
+const getPlatformBySlug = function(slug) {
+  return window.supportedPlatforms.find(p => p.slug === slug);
+};
+
+// Update UI state to show content for a given platform.
+//
+//  slug - slug matching a platform in data/platforms.yml
+//
+// Returns nothing.
 const showPlatform = function(slug) {
-  const platform = window.supportedPlatforms.find(p => p.slug === slug);
+  const platform = getPlatformBySlug(slug);
   if (!platform) return;
 
   const $contentBlocks = $('[data-platform-specific-content]');
 
+  // Update UI
   $contentBlocks.each((i, el) => {
     const $block = $(el);
     // Update dropdown target title
@@ -34,6 +49,32 @@ const showPlatform = function(slug) {
   });
 };
 
+// Add the current platform to all links on the page.
+//
+//  slug - slug matching a platform in data/platforms.yml
+//
+// Returns nothing.
+const addPlatformToLinks = function(slug) {
+  const platform = getPlatformBySlug(slug);
+  if (!platform) return;
+
+  $('a').each((i, el) => {
+    const $el = $(el);
+    const href = $el.attr('href');
+    const isDocLink = /(^\/|docs.sentry.io)/.test(href);
+    if (isDocLink) {
+      const { url, query } = qs.parseUrl(href);
+      query.platform = platform.slug;
+      $el.attr('href', `${url}?${qs.stringify(query)}`);
+    }
+  });
+};
+
+// Attach event listeners and set UI state based on the platform supplied via
+// query param, stored in localStorage as the preferred platform, or the
+// default platform, in that order.
+//
+// Returns nothing.
 const init = function() {
   $(document).on('click', '[data-toggle="platform"]', function(event) {
     event.preventDefault();
@@ -46,6 +87,7 @@ const init = function() {
   const queryPlatform = qs.parse(location.search).platform;
   const preferredPlatform = localStorage.getItem(KEY);
   showPlatform(queryPlatform || preferredPlatform);
+  addPlatformToLinks(queryPlatform);
 };
 
 export default { init };
