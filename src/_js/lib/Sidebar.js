ae717de17662702798acdcbc7dbb8d0d8c620c08
cameronmcefee <cm@cameronmcefee.com>
2018-09-12 12:08:59 -0700

    ref: simplify sidebar logic to fix bugs

--- src/_js/lib/Sidebar.js
+++ src/_js/lib/Sidebar.js
@@ -1,46 +1,40 @@
-const filterDepth = depth => {
-  return (i, el) => {
-    const $parents = $(el).parentsUntil(
-      '[data-sidebar-root]',
-      '[data-sidebar-branch]'
-    );
-    return $parents.length === depth;
-  };
-};
-
 $(document).on('page.didUpdate', function(event) {
   const $links = $('[data-sidebar-link]');
+  const $sections = $('[data-sidebar-root] > [data-sidebar-tree]').find(
+    '> [data-sidebar-branch]'
+  );
+
   let $active = $links.filter(`[href="${location.pathname}"]`).last();
-  const $categoryTree = $('[data-sidebar-tree]').filter(filterDepth(1));
-  $active = !!$active.length
-    ? $active
-    : $categoryTree.first().siblings('[data-sidebar-link]');
+
+  if (!$active.length) {
+    $active = $sections
+      .first()
+      .find('> [data-sidebar-tree]')
+      .find('> [data-sidebar-branch]')
+      .first()
+      .find('> [data-sidebar-link]');
+  }
+
   $links.each((i, el) => {
     const $el = $(el);
     $el.toggleClass('active', $el.is($active));
   });
-  const $sectionTree = $('[data-sidebar-branch]').filter(filterDepth(0));
-  const $activeBranch = $active.closest('[data-sidebar-branch]');
+
   $('[data-sidebar-branch]').each((i, el) => {
     const $branch = $(el);
-    const $link = $branch.find('> [data-sidebar-link]');
-    const $tree = $branch.find('> [data-sidebar-tree]');
+    const $parentBranch = $branch
+      .parent()
+      .closest('[data-sidebar-branch],[data-sidebar-root]');
     const hideWhenNoActiveChild =
       $branch.data('hide-when-inactive') !== undefined;
-    const containsActive = !!$tree.has($active).length;
-    const isActive = $branch.is($activeBranch);
-    const isSiblingOfActive = !!$branch.siblings().is($activeBranch);
-    const isChildOfActive = $branch
-      .parent()
-      .closest('[data-sidebar-branch]')
-      .is($activeBranch);
-    const isSection = $sectionTree.is($branch);
+    const parentTreeContainsActive = $parentBranch
+      .find('[data-sidebar-link]')
+      .is($active);
+
+    const containsActive = $branch.find('[data-sidebar-link]').is($active);
+
     const leaveVisble =
-      (isSection && !hideWhenNoActiveChild) ||
-      isActive ||
-      isSiblingOfActive ||
-      isChildOfActive ||
-      containsActive;
+      containsActive || (parentTreeContainsActive && !hideWhenNoActiveChild);
     $branch.toggleClass('collapse', !leaveVisble);
   });
 });
