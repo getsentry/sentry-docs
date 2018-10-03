50a09f26961f0c60b7e71c1e1ac373a6ec08475b
cameronmcefee <cm@cameronmcefee.com>
2018-10-03 16:08:48 -0700

    Refactor and test User

--- src/_js/lib/User.js
+++ src/_js/lib/User.js
@@ -5,22 +5,65 @@ import { logPageview } from './Page';
 export default class User {
   constructor() {
     this.namespace = 'docsUserDataV1';
-    this.userData = {
-      projects: [constructDSNObject()],
-      projectPref: -1
-    };
-
     this.init = this.init.bind(this);
     this.update = this.update.bind(this);
+    this.setUserData = this.setUserData.bind(this);
+    this.getUserData = this.getUserData.bind(this);
   }
 
-  update(newData = {}) {
-    this.userData = Object.assign({}, this.userData, newData);
-    localStorage.setItem(this.namespace, JSON.stringify(this.userData));
-    $(document).trigger('user.didUpdate', [this]);
+  update() {
+    $(document).trigger('user.didUpdate', [this.getUserData()]);
+  }
+
+  setUserData(newData = {}) {
+    let userData = {};
+
+    try {
+      userData = JSON.parse(localStorage.getItem(this.namespace)) || {};
+    } catch (error) {
+      // Don't worry about errors if localStorage is corrupt, we'll just discard
+      // the object and start fresh.
+    }
+
+    const { projects: tmpProjects, preferred: tmpPref } = {
+      ...userData,
+      ...newData
+    };
+
+    const projects =
+      tmpProjects && tmpProjects.length ? tmpProjects : [defaultProject];
+
+    // Default to the first project
+    let preferred = projects[0];
+    if (tmpPref) {
+      // If a preference is set, check it still exists, otherwise still use
+      // the first project as the default.
+      preferred = projects.find(({ id }) => id === tmpPref.id) || preferred;
+    }
+
+    const payload = { projects, preferred };
+    localStorage.setItem(this.namespace, JSON.stringify(payload));
+    return payload;
+  }
+
+  getUserData() {
+    let data = null;
+    try {
+      data = JSON.parse(localStorage.getItem(this.namespace));
+    } catch (error) {
+      // If the parse failed, we'll just purge the data
+    }
+
+    const { preferred, projects } = data || this.setUserData();
+    return {
+      preferred: constructDSNObject(preferred),
+      projects: projects.map(constructDSNObject)
+    };
   }
 
   init() {
+    $('[data-hide-when-logged-in]').addClass('d-none');
+
     const cached = JSON.parse(localStorage.getItem(this.namespace));
     if (cached) this.update(cached);
 
@@ -32,25 +75,18 @@ export default class User {
         withCredentials: true
       }
     })
-      .done(({ projects, api_keys, user }) => {
+      .then(({ projects, user }) => {
         window.ra.identify(user.id);
         window.amplitude.getInstance().setUserId(user.id);
-        const userData = { ...this.userData };
-        if (projects && projects.length) {
-          userData.projects = projects.map(constructDSNObject);
-          if (userData.projectPref === -1)
-            userData.projectPref = projects[0].id;
-        }
-        $('[data-hide-when-logged-in]').toggleClass(
-          'd-none',
-          user.isAuthenticated
-        );
-        this.update(userData);
-      })
-      .fail(() => {
+        const { isAuthenticated } = user;
+        $('[data-hide-when-logged-in]').toggleClass('d-none', isAuthenticated);
+        this.setUserData({ projects });
         this.update();
       })
-      .always(() => {
+      .catch(error => {
+        this.update();
+      })
+      .then(() => {
         logPageview();
       });
   }
@@ -73,38 +109,38 @@ const formatAPIURL = function(dsn) {
   return `${dsn.scheme}${dsn.host}/api`;
 };
 
-const constructDSNObject = function(project = {}) {
-  let dsn;
-  if (project.dsn) {
-    // Transform the dsn into useful values
-    const match = project.dsn.match(/^(.*?\/\/)(.*?):(.*?)@(.*?)(\/.*?)$/);
+export const defaultProject = {
+  projectName: 'your-project',
+  secretKey: '<secret>',
+  publicKey: '<key>',
+  dsnPublic: 'https://<key>@sentry.io/<project>',
+  id: -1,
+  dsn: 'https://<key>:<secret>@sentry.io/<project>',
+  organizationName: 'Example',
+  organizationSlug: 'your-org',
+  projectSlug: 'your-project'
+};
 
-    dsn = {
-      scheme: escape(match[1]),
-      publicKey: escape(match[2]),
-      secretKey: `${escape(match[3])}`,
-      host: escape(match[4]),
-      pathSection: escape(match[5])
-    };
-  } else {
-    dsn = {
-      scheme: 'https://',
-      publicKey: '&lt;key&gt;',
-      secretKey: '&lt;secret&gt;',
-      host: 'sentry.io',
-      pathSection: '/&lt;project&gt;'
-    };
-  }
+export const constructDSNObject = function(project = {}) {
+  project = { ...defaultProject, ...project };
+  // Transform the dsn into useful values
+  const match = project.dsn.match(/^(.*?\/\/)(.*?):(.*?)@(.*?)(\/.*?)$/);
 
-  const projectID = project && project.id ? project.id.toString() : null
+  const dsn = {
+    scheme: escape(match[1]),
+    publicKey: escape(match[2]),
+    secretKey: `${escape(match[3])}`,
+    host: escape(match[4]),
+    pathSection: escape(match[5])
+  };
 
   return {
-    id: project.id || -1,
-    group: escape(project.organizationName) || 'Example',
-    PROJECT_NAME: escape(project.projectSlug) || 'your-project',
-    PROJECT_ID: projectID || '&lt;project-id&gt;',
-    ORG_NAME: escape(project.organizationSlug) || 'your-org',
-    DSN: formatDsn(dsn, { public: false }),
+    id: project.id,
+    group: escape(project.organizationName),
+    PROJECT_NAME: escape(project.projectSlug),
+    PROJECT_ID: project.id.toString(),
+    ORG_NAME: escape(project.organizationSlug),
+    DSN: formatDsn(dsn),
     PUBLIC_DSN: formatDsn(dsn, { public: true }),
     PUBLIC_KEY: dsn.publicKey,
     SECRET_KEY: dsn.secretKey,
