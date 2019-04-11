a101460d9bd2dcb7ebc1c7e2faeab4a77b13ea38
adhiraj <693121+adhiraj@users.noreply.github.com>
2019-04-10 21:25:54 -0700

    Add docs analytics to another GA property (#889)
    * Add docs analytics to another GA property
    * Remove FB and hubspot
    * Don't log GA twice if the user has already consented
    * Make analytics work with SPA

--- src/_js/lib/Page.js
+++ src/_js/lib/Page.js
@@ -1,5 +1,19 @@
 import { canonicalPath, canonicalUrl } from './Helpers';
 
+export const logGooglePageview = function() {
+  if (!window.gtag) return;
+
+  const i = location.href.indexOf('#');
+  const url = i === -1 ? location.href : location.href.slice(0, i);
+  const payload = {
+    anonymize_ip: true,
+    page_title: document.title,
+    page_location: url,
+  };
+  window.gtag('config', 'UA-30327640-1', payload);
+  window.gtag('config', 'UA-30327640-3', payload);
+};
+
 export const logPageview = function() {
   window.amplitude.getInstance().logEvent('Viewed Docs Page', {
     path: canonicalPath(),
@@ -9,4 +23,5 @@ export const logPageview = function() {
     url: canonicalUrl(location.search)
   });
   window.ra.page();
+  logGooglePageview();
 };
