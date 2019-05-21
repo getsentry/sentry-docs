e7419119303f37771bf2fb3dfe512ce25cbf8086
David Cramer <dcramer@gmail.com>
2019-05-21 11:25:09 -0700

    ref: Sort platform selector

--- src/_plugins/ci_sort_filter.rb
+++ src/_plugins/ci_sort_filter.rb
@@ -1,7 +1,11 @@
 module Jekyll
   module CaseInsensitiveSortFilter
-    def ci_sort(input, arg)
-      input.sort_by { |x| (x[arg] || '').downcase }
+    def ci_sort(input, arg=nil)
+      unless arg.nil?
+        input.sort_by { |x| (x[arg] || '').downcase }
+      else
+        input.sort_by { |x| (x || '').downcase }
+      end
     end
   end
 end
