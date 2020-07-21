77d8187c12312c21a876b074afafa43a47e8ca45
David Cramer <dcramer@gmail.com>
2020-07-20 18:10:55 -0700

    fix: Default all links to remote until Jekyll is gone
    Otherwise Gatsby will attempt to load links even when they're not valid Gatsby pages.

--- gatsby/src/components/smartLink.js
+++ gatsby/src/components/smartLink.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from "react";
+import { Link } from "gatsby";
 
-import ExternalLink from './externalLink';
+import ExternalLink from "./externalLink";
 
 // TODO(dcramer): when Gatsby conversion is done, remove remote default
-export default ({ to, href, children, remote, ...props }) => {
-  const realTo = to || href || '';
-  if (realTo.indexOf('://') !== -1) {
+export default ({ to, href, children, remote = true, ...props }) => {
+  const realTo = to || href || "";
+  if (realTo.indexOf("://") !== -1) {
     return (
       <ExternalLink href={realTo} {...props}>
         {children}
       </ExternalLink>
     );
-  } else if (realTo.indexOf('/') !== 0 || remote) {
+  } else if (realTo.indexOf("/") !== 0 || remote) {
     // this handles cases like anchor tags (where Link messes thats up)
     return (
       <a href={realTo} {...props}>
