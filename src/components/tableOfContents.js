b559544be731e98b9c9d1726e404102bcf736442
David Cramer <dcramer@gmail.com>
2020-08-01 09:59:23 -0700

    Remove named anchors from ToC

--- src/components/tableOfContents.js
+++ src/components/tableOfContents.js
@@ -5,10 +5,12 @@ export default ({ toc: { items } }) => {
 
   const recurse = items =>
     items.map(i => {
+      // The title is included the explicit anchor name when present. So lets strip it.
+      // e.g. My Title {#my-title}
       if (!i.title) return recurse(i.items);
       return (
         <li className="toc-entry" key={i.url}>
-          <a href={i.url}>{i.title}</a>
+          <a href={i.url}>{i.title.replace(/\s\{\#[^\}]+\}$/, "")}</a>
           {i.items && <ul>{recurse(i.items)}</ul>}
         </li>
       );
