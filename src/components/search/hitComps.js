a06763e9550b861c499008f77e28491a10025788
David Cramer <dcramer@gmail.com>
2020-08-11 19:32:35 -0700

    Add url when category isnt available

--- src/components/search/hitComps.js
+++ src/components/search/hitComps.js
@@ -7,11 +7,15 @@ export const PageHit = clickHandler => ({ hit }) => {
     <Link to={hit.url} onClick={clickHandler}>
       <h6 className="mb-1">
         <Highlight attribute="title" hit={hit} tagName="mark" />
-        {hit.categories && (
+        {!!hit.categories ? (
           <React.Fragment>
             {hit.categories.map(category => (
               <span className="badge badge-secondary">{category}</span>
             ))}
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <span className="badge badge-secondary">{hit.url}</span>
           </React.Fragment>
         )}
       </h6>
