0d421a4aef5450b94d02980e29b34aab3a189bf2
David Cramer <dcramer@gmail.com>
2020-09-02 15:47:28 -0700

    Unify sign in display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -56,17 +56,21 @@ const IndexPage = () => {
             </a>
             <ul className="navbar-nav">
               <li className="nav-item" data-hide-when-logged-in>
-                <a className="nav-link" href={`https://sentry.io/auth/login/`}>
-                  Sign in
-                </a>
-              </li>
-              <li className="nav-item" data-hide-when-logged-in>
-                <a
-                  href={`https://sentry.io/signup/`}
-                  className="btn btn-primary d-block d-inline-block-md"
-                  role="button"
-                >
-                  Get started
+                <a className="nav-link" href="https://sentry.io/">
+                  Sign In
+                  <svg
+                    width="1em"
+                    height="1em"
+                    viewBox="0 0 16 16"
+                    className="bi bi-arrow-right-short"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
+                    />
+                  </svg>
                 </a>
               </li>
             </ul>
