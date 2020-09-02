16dea58011d1daefe4c3448fcdfb314009e4b9f4
David Cramer <dcramer@gmail.com>
2020-09-02 16:24:01 -0700

    Dont use SmartLink due to external link icon

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -72,7 +72,7 @@ export default ({ platforms }: Props): JSX.Element => {
             </SmartLink>
           </Nav.Item>
           <Nav.Item>
-            <SmartLink className="nav-link" to="https://sentry.io/">
+            <Nav.Link href="https://sentry.io/">
               Sign In
               <svg
                 width="1em"
@@ -87,7 +87,7 @@ export default ({ platforms }: Props): JSX.Element => {
                   d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
                 />
               </svg>
-            </SmartLink>
+            </Nav.Link>
           </Nav.Item>
         </Nav>
       </div>
