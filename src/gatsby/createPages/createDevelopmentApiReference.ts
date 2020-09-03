07f7019c1d65ef19ab827ea38742251b420ca5c6
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-09-03 15:44:08 -0700

    feat: Reuse existing API reference pages

--- src/gatsby/createPages/createDevelopmentApiReference.ts
+++ src/gatsby/createPages/createDevelopmentApiReference.ts
@@ -1,12 +1,65 @@
+import { getChild, getDataOrPanic } from "../helpers";
+
 export default async ({ actions, graphql, reporter }) => {
-  const component = require.resolve(
-    `../../templates/developmentApiReference.js`
+  const data = await getDataOrPanic(
+    `
+          query {
+            allFile(filter: {absolutePath: {}, relativePath: {in: ["permissions.mdx", "auth.mdx", "index.mdx", "requests.mdx"]}, dir: {regex: "/api/"}}) {
+              nodes {
+                id
+                childMarkdownRemark {
+                  frontmatter {
+                    title
+                    description
+                    draft
+                    noindex
+                    sidebar_order
+                    redirect_from
+                  }
+                  fields {
+                    slug
+                    legacy
+                  }
+                  excerpt(pruneLength: 5000)
+                }
+                childMdx {
+                  frontmatter {
+                    title
+                    description
+                    draft
+                    noindex
+                    sidebar_order
+                    redirect_from
+                  }
+                  fields {
+                    slug
+                    legacy
+                  }
+                  excerpt(pruneLength: 5000)
+                }
+              }
+            }
+          }
+        `,
+    graphql,
+    reporter
   );
-  await actions.createPage({
-    path: `/development-api/`,
-    component,
-    context: {
-      title: "API Reference"
-    },
+
+  const component = require.resolve(`../../templates/doc.tsx`);
+  data.allFile.nodes.map((node: any) => {
+    const child = getChild(node);
+    if (child && child.fields) {
+      console.log({ path: child.fields.slug });
+      actions.createPage({
+        path: `development-api${child.fields.slug}`,
+        component,
+        context: {
+          excerpt: child.excerpt,
+          ...child.frontmatter,
+          id: node.id,
+          legacy: child.fields.legacy,
+        },
+      });
+    }
   });
 };
