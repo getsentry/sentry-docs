e3125885a192000fbd41492c7db75e98a9e47635
Armin Ronacher <armin.ronacher@active-4.com>
2020-09-03 23:18:28 +0200

    feat: improved redirecting code

--- plugins/gatsby-redirects/gatsby-node.js
+++ plugins/gatsby-redirects/gatsby-node.js
@@ -1,12 +1,14 @@
 const { NginxConfFile } = require("nginx-conf");
 const { remove } = require("fs-extra");
+const { resolve } = require("url");
 
 exports.onCreatePage = ({ page, reporter, actions: { createRedirect } }) => {
   if (page.context && page.context.redirect_from) {
     page.context.redirect_from.forEach(fromPath => {
-      reporter.verbose(`Adding redirect from ${fromPath} to ${page.path}`);
+      let realFromPath = resolve(page.path, fromPath);
+      reporter.verbose(`Adding redirect from ${realFromPath} to ${page.path}`);
       createRedirect({
-        fromPath,
+        fromPath: realFromPath,
         toPath: page.path,
         redirectInBrowser: true,
         isPermanent: true,
