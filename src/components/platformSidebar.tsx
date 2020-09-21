784b30a45a63bece60b345b10fc9c66ab0b536d1
David Cramer <dcramer@gmail.com>
2020-09-21 15:37:47 -0700

    ref: Updated global hierarchy

--- src/components/platformSidebar.tsx
+++ src/components/platformSidebar.tsx
@@ -72,24 +72,28 @@ export const PlatformSidebar = ({
         root={pathRoot}
         tree={tree}
         title={`Sentry for ${(guide || platform).title}`}
-        showDepth={1}
         prependLinks={[[`/${pathRoot}/`, "Getting Started"]]}
         exclude={[
           `/${pathRoot}/enriching-error-data/`,
           `/${pathRoot}/data-management/`,
+          `/${pathRoot}/performance/`,
           `/${pathRoot}/guides/`,
         ]}
       />
       <DynamicNav
+        root={`/${pathRoot}/performance`}
+        title="Performance Monitoring"
+        prependLinks={[[`/${pathRoot}/performance/`, "Enable Performance"]]}
+        tree={tree}
+      />
+      <DynamicNav
         root={`/${pathRoot}/enriching-error-data`}
         title="Enriching Error Data"
-        showDepth={1}
         tree={tree}
       />
       <DynamicNav
         root={`/${pathRoot}/data-management`}
         title="Data Management"
-        showDepth={1}
         tree={tree}
       />
       <DynamicNav
