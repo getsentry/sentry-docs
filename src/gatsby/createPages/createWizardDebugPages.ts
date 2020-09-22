67e9fa779da8c22031fa8e44d7309859b6063f78
David Cramer <dcramer@gmail.com>
2020-09-22 11:56:34 -0700

    ref: Improve debugability of wizard

--- src/gatsby/createPages/createWizardDebugPages.ts
+++ src/gatsby/createPages/createWizardDebugPages.ts
@@ -7,6 +7,7 @@ export default async ({ actions, graphql, reporter }) => {
         allFile(filter: { sourceInstanceName: { eq: "wizard" } }) {
           nodes {
             id
+            relativePath
             childMarkdownRemark {
               frontmatter {
                 title
@@ -38,6 +39,11 @@ export default async ({ actions, graphql, reporter }) => {
   const component = require.resolve(`../../templates/wizardDebug.tsx`);
   data.allFile.nodes.map((node: any) => {
     const child = getChild(node);
+    if (!child) {
+      throw new Error(
+        `Wziard cannot find child for ${node.id} = ${node.relativePath}`
+      );
+    }
     actions.createPage({
       path: `/_debug/wizard${child.fields.slug}`,
       component,
