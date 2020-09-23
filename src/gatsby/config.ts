3678c7d7c0c62d8a78c0b2fe51560db21e245300
Colleen O'Rourke <colleen@sentry.io>
2020-09-23 16:20:12 -0700

    feat(api-docs): Update SHA to get latest schema

--- src/gatsby/config.ts
+++ src/gatsby/config.ts
@@ -192,7 +192,7 @@ const getPlugins = () => {
         name: "openapi",
         resolve: async () => {
           const response = await axios.get(
-            "https://raw.githubusercontent.com/getsentry/sentry-api-schema/fd63b135165bc5f8292291ecd69996a63f098f98/openapi-derefed.json"
+            "https://raw.githubusercontent.com/getsentry/sentry-api-schema/2006eb13d297386585507f6f5c40e9a7306a2835/openapi-derefed.json"
           );
           return response.data;
         },
