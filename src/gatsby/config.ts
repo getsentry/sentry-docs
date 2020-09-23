16977b8cde863ae0ec689a5356839defe5ee81d0
Colleen O'Rourke <colleen@sentry.io>
2020-09-23 13:50:44 -0700

    feat(api-docs): Update SHA one last time

--- src/gatsby/config.ts
+++ src/gatsby/config.ts
@@ -192,7 +192,7 @@ const getPlugins = () => {
         name: "openapi",
         resolve: async () => {
           const response = await axios.get(
-            "https://raw.githubusercontent.com/getsentry/sentry-api-schema/57538f520d7412c8436272f0455c37a2367e4d12/openapi-derefed.json"
+            "https://raw.githubusercontent.com/getsentry/sentry-api-schema/fd63b135165bc5f8292291ecd69996a63f098f98/openapi-derefed.json"
           );
           return response.data;
         },
