84b8b09b1e06119485ff7402a628228e6853ad8e
David Cramer <dcramer@gmail.com>
2020-09-29 15:26:02 -0700

    Revert "feat: Switch to fixed navs with scrolling (#2387)"
    This reverts commit a3a507cb7846df70c5d677b510af7d4230687548.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Breadcrumbs from "./breadcrumbs";
 import Header from "./header";
 import Sidebar from "./sidebar";
+import Navbar from "./navbar";
 
 import "~src/css/screen.scss";
 
@@ -22,34 +23,41 @@ export default ({
   sidebar,
   pageContext = {},
 }: Props): JSX.Element => {
-  const hasSidebar = !!sidebar;
-
   return (
-    <div className={`document-wrapper ${hasSidebar ? "with-sidebar" : ""}`}>
-      <Header
-        {...(pageContext.platform && {
-          platforms: [pageContext.platform.name],
-        })}
-      />
-      <main role="main">
-        <section className="pt-3 px-3 content-max prose">
-          <div className="pb-3">
-            <Breadcrumbs />
-          </div>
-          {children}
-        </section>
-      </main>
+    <div className="document-wrapper">
+      <div className="sidebar">
+        <Header />
 
-      <div
-        className="sidebar d-md-flex flex-column align-items-stretch collapse navbar-collapse"
-        id="sidebar"
-      >
-        <div className="toc">
-          <div className="text-white p-3">
-            {sidebar ? sidebar : <Sidebar />}
+        <div
+          className="d-md-flex flex-column align-items-stretch collapse navbar-collapse"
+          id="sidebar"
+        >
+          <div className="toc">
+            <div className="text-white p-3">
+              {sidebar ? sidebar : <Sidebar />}
+            </div>
           </div>
         </div>
       </div>
+
+      <main role="main" className="px-0">
+        <div className="flex-grow-1">
+          <div className="d-none d-md-block">
+            <Navbar
+              {...(pageContext.platform && {
+                platforms: [pageContext.platform.name],
+              })}
+            />
+          </div>
+
+          <section className="pt-3 px-3 content-max prose">
+            <div className="pb-3">
+              <Breadcrumbs />
+            </div>
+            {children}
+          </section>
+        </div>
+      </main>
     </div>
   );
 };
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -17,80 +17,82 @@ export default ({ platforms }: Props): JSX.Element => {
   const [currentPlatform] = usePlatform(null, false);
 
   return (
-    <nav className="collapse navbar-collapse content-max" id="navbar-menu">
-      <Search path={location.pathname} platforms={platforms} />
-      <Nav className="justify-content-end" style={{ flex: 1 }}>
-        <Nav.Item>
-          <SmartLink className="nav-link" to="/product/">
-            Product
-          </SmartLink>
-        </Nav.Item>
-        <NavDropdown
-          title={
-            currentPlatform ? (
-              <React.Fragment>
+    <div className="navbar navbar-expand-md navbar-light global-header">
+      <div className="collapse navbar-collapse content-max" id="navbar-menu">
+        <Search path={location.pathname} platforms={platforms} />
+        <Nav className="justify-content-end" style={{ flex: 1 }}>
+          <Nav.Item>
+            <SmartLink className="nav-link" to="/product/">
+              Product
+            </SmartLink>
+          </Nav.Item>
+          <NavDropdown
+            title={
+              currentPlatform ? (
+                <React.Fragment>
+                  <PlatformIcon
+                    platform={currentPlatform.key}
+                    size={16}
+                    style={{ marginRight: "0.5rem" }}
+                    format="lg"
+                  />
+                  {currentPlatform.title}
+                </React.Fragment>
+              ) : (
+                "Platforms"
+              )
+            }
+            id="platforms"
+          >
+            {platformList.map(platform => (
+              <SmartLink
+                className={`dropdown-item ${
+                  currentPlatform && currentPlatform.key == platform.key
+                    ? "active"
+                    : ""
+                }`}
+                key={platform.key}
+                to={platform.url}
+              >
                 <PlatformIcon
-                  platform={currentPlatform.key}
+                  platform={platform.key}
                   size={16}
                   style={{ marginRight: "0.5rem" }}
                   format="lg"
                 />
-                {currentPlatform.title}
-              </React.Fragment>
-            ) : (
-              "Platforms"
-            )
-          }
-          id="platforms"
-        >
-          {platformList.map(platform => (
-            <SmartLink
-              className={`dropdown-item ${
-                currentPlatform && currentPlatform.key == platform.key
-                  ? "active"
-                  : ""
-              }`}
-              key={platform.key}
-              to={platform.url}
-            >
-              <PlatformIcon
-                platform={platform.key}
-                size={16}
-                style={{ marginRight: "0.5rem" }}
-                format="lg"
-              />
-              {platform.title}
+                {platform.title}
+              </SmartLink>
+            ))}
+            <NavDropdown.Divider />
+            <SmartLink className="dropdown-item" to="/platforms/">
+              Show all platforms
             </SmartLink>
-          ))}
-          <NavDropdown.Divider />
-          <SmartLink className="dropdown-item" to="/platforms/">
-            Show all platforms
-          </SmartLink>
-        </NavDropdown>
-        <Nav.Item>
-          <SmartLink className="nav-link" to="/api/">
-            API
-          </SmartLink>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="https://sentry.io/">
-            Sign In
-            <svg
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              className="bi bi-arrow-right-short"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-              />
-            </svg>
-          </Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </nav>
+          </NavDropdown>
+          <Nav.Item>
+            <SmartLink className="nav-link" to="/api/">
+              API
+            </SmartLink>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="https://sentry.io/">
+              Sign In
+              <svg
+                width="1em"
+                height="1em"
+                viewBox="0 0 16 16"
+                className="bi bi-arrow-right-short"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
+                />
+              </svg>
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
+    </div>
   );
 };
