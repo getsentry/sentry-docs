cad6d70a128e7ed0b3f5434a5b2ccd260ba7cde2
David Cramer <dcramer@gmail.com>
2020-09-29 15:26:02 -0700

    Revert "feat: Switch to fixed navs with scrolling (#2387)"
    This reverts commit a3a507cb7846df70c5d677b510af7d4230687548.

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -2,15 +2,6 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Header from "../header";
 
-jest.mock("../navbar", () => {
-  return {
-    __esModule: true,
-    default: () => {
-      return <div />;
-    },
-  };
-});
-
 describe("Header", () => {
   it("renders correctly", () => {
     const tree = renderer.create(<Header />).toJSON();
