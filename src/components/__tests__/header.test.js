611e26895c67ca31a07fb998c33410943080b6a4
David Cramer <dcramer@gmail.com>
2020-09-29 10:58:32 -0700

    feat: Switch to fixed navs with scrolling

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import renderer from "react-test-renderer";
 import Header from "../header";
 
+jest.mock("../navbar", () => {
+  return {
+    __esModule: true,
+    default: () => {
+      return <div />;
+    },
+  };
+});
+
 describe("Header", () => {
   it("renders correctly", () => {
     const tree = renderer.create(<Header />).toJSON();
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,38 +1,43 @@
 import React from "react";
 
+import Navbar from "./navbar";
 import SmartLink from "./smartLink";
 
-export default (): JSX.Element => {
+type Props = {
+  platforms?: string[];
+};
+
+export default ({ platforms }: Props): JSX.Element => {
   return (
-    <div className="navbar navbar-expand-md navbar-light bg-white global-header">
-      <SmartLink
-        to="/"
-        title="Sentry error monitoring"
-        className="navbar-brand pb-0"
-      >
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
-          <g height="75" width="75" className="loader-spin">
-            <path
-              d="M7.8 49.78c-1.75 2.88-3.19 5.4-4.35 7.56a3.9 3.9 0 0 0 3.34 6h18.86a25.75 25.75 0 0 0-12.87-22.19c1.9-3.17 5.12-9 6.32-11a38.47 38.47 0 0 1 19.14 33.23h12.63a50.79 50.79 0 0 0-25.51-44C29.65 12 32.38 7 33.89 4.64a4 4 0 0 1 6.66 0C42 7 69.53 54.8 71 57.34a4 4 0 0 1-3.75 6h-6.79"
-              fill="none"
-              stroke="currentColor"
-              className="loader-stroke loading"
-              strokeWidth="5"
-            />
-          </g>
-        </svg>
-        <h6>Docs</h6>
-      </SmartLink>
+    <header className="navbar navbar-expand-md navbar-light bg-white global-header">
+      <div className="navbar-brand">
+        <SmartLink to="/" title="Sentry error monitoring">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 75 75">
+            <g height="75" width="75" className="loader-spin">
+              <path
+                d="M7.8 49.78c-1.75 2.88-3.19 5.4-4.35 7.56a3.9 3.9 0 0 0 3.34 6h18.86a25.75 25.75 0 0 0-12.87-22.19c1.9-3.17 5.12-9 6.32-11a38.47 38.47 0 0 1 19.14 33.23h12.63a50.79 50.79 0 0 0-25.51-44C29.65 12 32.38 7 33.89 4.64a4 4 0 0 1 6.66 0C42 7 69.53 54.8 71 57.34a4 4 0 0 1-3.75 6h-6.79"
+                fill="none"
+                stroke="currentColor"
+                className="loader-stroke loading"
+                strokeWidth="5"
+              />
+            </g>
+          </svg>
+          <h6>Docs</h6>
+        </SmartLink>
+      </div>
+
+      <Navbar platforms={platforms} />
 
       <button
         className="d-md-none btn btn-outline-dark"
         type="button"
         onClick={() => {
           const el = document.getElementById("sidebar");
-          if (el.style.display === "block") {
+          if (el.style.display === "flex") {
             el.style.display = "none";
           } else {
-            el.style.display = "block";
+            el.style.display = "flex";
           }
         }}
         aria-controls="sidebar"
@@ -41,6 +46,6 @@ export default (): JSX.Element => {
       >
         Table of Contents
       </button>
-    </div>
+    </header>
   );
 };
