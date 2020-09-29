e4349b92718d1525a62155e4372e7375f0ed1ab4
David Cramer <dcramer@users.noreply.github.com>
2020-09-29 09:41:40 -0700

    feat: Add auto generated next steps to PHP (#2386)
    - Add nextPages and header attributes to PageGrid
    - Add sidebar_order to all sections
    - Add descriptions for various pages
    
    Co-authored-by: Fiona <61481573+PeloWriter@users.noreply.github.com>

--- src/components/pageGrid.tsx
+++ src/components/pageGrid.tsx
@@ -20,34 +20,51 @@ const query = graphql`
   }
 `;
 
-export default (): JSX.Element => {
+type Props = {
+  nextPages: boolean;
+  header?: string;
+};
+
+export default ({ nextPages = false, header }: Props): JSX.Element => {
   const data = useStaticQuery(query);
   const location = useLocation();
 
   const currentPath = location.pathname;
   const currentPathLen = currentPath.length;
 
-  const matches = sortPages(
+  let matches = sortPages(
     data.allSitePage.nodes.filter(
       n =>
         n.context &&
         n.context.title &&
-        n.path !== currentPath &&
         n.path.indexOf(currentPath) === 0 &&
         n.path.slice(currentPathLen).split("/", 2)[1] === ""
     )
   );
 
+  if (nextPages) {
+    matches = matches.slice(
+      matches.indexOf(matches.find(n => n.path === currentPath))
+    );
+  } else {
+    matches = matches.filter(n => n.path !== currentPath);
+  }
+
+  if (!matches.length) return null;
+
   return (
-    <ul>
-      {matches.map(n => (
-        <li key={n.path} style={{ marginBottom: "1rem" }}>
-          <h4 style={{ marginBottom: 0 }}>
-            <SmartLink to={n.path}>{n.context.title}</SmartLink>
-          </h4>
-          {n.context.description ?? <p>{n.context.description}</p>}
-        </li>
-      ))}
-    </ul>
+    <nav>
+      {header && <h2>{header}</h2>}
+      <ul>
+        {matches.map(n => (
+          <li key={n.path} style={{ marginBottom: "1rem" }}>
+            <h4 style={{ marginBottom: 0 }}>
+              <SmartLink to={n.path}>{n.context.title}</SmartLink>
+            </h4>
+            {n.context.description ?? <p>{n.context.description}</p>}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
