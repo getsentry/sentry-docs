e7dfe5f361e53c12e977d663af2dfdc0944a023c
David Cramer <dcramer@gmail.com>
2020-10-01 13:33:24 -0700

    feat: Collapse Python into common content; add tracing

--- src/components/guideGrid.tsx
+++ src/components/guideGrid.tsx
@@ -2,22 +2,22 @@ import React from "react";
 import PlatformIcon from "platformicons";
 
 import SmartLink from "./smartLink";
-import { getPlatform, Platform } from "./hooks/usePlatform";
+import usePlatform, { Platform } from "./hooks/usePlatform";
 
 type Props = {
-  platform: string;
+  platform?: string;
 };
 
 export default ({ platform }: Props): JSX.Element => {
-  const currentPlatform = getPlatform(platform) as Platform;
+  const [currentPlatform] = usePlatform(platform);
   // platform might actually not be a platform, so lets handle that case gracefully
-  if (!currentPlatform.guides) {
+  if (!(currentPlatform as Platform).guides) {
     return null;
   }
 
   return (
     <ul>
-      {currentPlatform.guides.map(guide => (
+      {(currentPlatform as Platform).guides.map(guide => (
         <li key={guide.key}>
           <SmartLink to={guide.url}>
             <PlatformIcon
