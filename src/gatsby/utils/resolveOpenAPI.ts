41d3c0fadf1b38ca3b1ecb11ee5cf456d8236803
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2020-10-23 17:21:56 -0700

    feat(actions): Create new workflow to bump sentry-api-schema SHA (#2563)
    Refactor our existing bump SHA action in sentry-api-docs, so that we have something similar to ./bin/bump-sentry in getsentry.
    This workflow listens to a workflow dispatch event and takes in a SHA. Then it will search and replace the with this latest SHA.

--- src/gatsby/utils/resolveOpenAPI.ts
+++ src/gatsby/utils/resolveOpenAPI.ts
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { promises as fs } from "fs";
 
+// SENTRY_API_SCHEMA_SHA is used in the bump-openapi GHA workflow.
+// DO NOT change variable name unless you change it in the bump-openapi GHA workflow.
+const SENTRY_API_SCHEMA_SHA = "20c924b1bb3ead4c76b540c075f59733590c0c3a";
+
 const activeEnv =
   process.env.GATSBY_ENV || process.env.NODE_ENV || "development";
 
@@ -18,7 +22,7 @@ export default async () => {
     }
   }
   const response = await axios.get(
-    "https://raw.githubusercontent.com/getsentry/sentry-api-schema/20c924b1bb3ead4c76b540c075f59733590c0c3a/openapi-derefed.json"
+    `https://raw.githubusercontent.com/getsentry/sentry-api-schema/${SENTRY_API_SCHEMA_SHA}/openapi-derefed.json`
   );
   return response.data;
 };
