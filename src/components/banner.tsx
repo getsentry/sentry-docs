1e4eb1706d927b7c00fb38b225e83da579085139
Fiona <61481573+PeloWriter@users.noreply.github.com>
2021-06-15 13:20:33 -0700

    add banner for June 2021 (#3742)

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -5,11 +5,11 @@ import React, { useState, useEffect } from "react";
 // This is a lazy way of doing things but will work until
 // we put a more robust solution in place.
 //
-const SHOW_BANNER = false;
+const SHOW_BANNER = true;
 const BANNER_TEXT =
-  "We don't like surveys either. But we do like socks.";
+  "Upcoming Technical Workshop: Next.js Apps";
 const BANNER_LINK_URL =
-  "https://www.surveymonkey.com/r/83BKWC7";
+  "https://us06web.zoom.us/webinar/register/5116227447463/WN_znhaqwALTXWf-3Rqtf8mBQ";
 const BANNER_LINK_TEXT = "Register here.";
 const OPTIONAL_BANNER_IMAGE = null;
 
