1f491c5449398e88e891280a29b5483ba363cf35
William Mak <william@wmak.io>
2022-06-03 18:49:40 -0400

    bump

--- src/gatsby/utils/resolveOpenAPI.ts
+++ src/gatsby/utils/resolveOpenAPI.ts
@@ -3,7 +3,7 @@ import { promises as fs } from "fs";
 
 // SENTRY_API_SCHEMA_SHA is used in the sentry-docs GHA workflow in getsentry/sentry-api-schema.
 // DO NOT change variable name unless you change it in the sentry-docs GHA workflow in getsentry/sentry-api-schema.
-const SENTRY_API_SCHEMA_SHA = "55581ea676af95f8ab43080b3dd929c93f4184f8"
+const SENTRY_API_SCHEMA_SHA = "803203504cbdf1881229c35024580d34f60d813f"
 
 const activeEnv =
   process.env.GATSBY_ENV || process.env.NODE_ENV || "development";
