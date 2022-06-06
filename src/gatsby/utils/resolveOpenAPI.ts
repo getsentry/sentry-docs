30cda0cce054980063f66489ec06bf59988a310e
William Mak <william@wmak.io>
2022-06-06 10:02:53 -0400

    chore(api): bump api sha with events doc (#5116)
    Bump the SENTRY_API_SCHEMA_SHA with latest changes

--- src/gatsby/utils/resolveOpenAPI.ts
+++ src/gatsby/utils/resolveOpenAPI.ts
@@ -3,7 +3,7 @@ import { promises as fs } from "fs";
 
 // SENTRY_API_SCHEMA_SHA is used in the sentry-docs GHA workflow in getsentry/sentry-api-schema.
 // DO NOT change variable name unless you change it in the sentry-docs GHA workflow in getsentry/sentry-api-schema.
-const SENTRY_API_SCHEMA_SHA = "d5ed0052c0935311db62a0f2bbf0728e0f503964"
+const SENTRY_API_SCHEMA_SHA = "803203504cbdf1881229c35024580d34f60d813f"
 
 const activeEnv =
   process.env.GATSBY_ENV || process.env.NODE_ENV || "development";
