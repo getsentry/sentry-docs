d864040dc2da0118ce2177f07a6faaf743a3e12d
dorothymlee <89860861+dorothymlee@users.noreply.github.com>
2022-08-04 10:08:56 -0700

    updated banner (#5379)

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -7,9 +7,9 @@ import React, { useState, useEffect } from "react";
 //
 const SHOW_BANNER = true;
 const BANNER_TEXT =
-  "Join us in sorting through the madness that is code, and finding new ways to improve workflow productivity at DEX 2022 on Sept. 28, 2022.";
+  "You've instrumented performance...now what? See what you can do in our interactive workshop on Aug 17 @ 10am PT";
 const BANNER_LINK_URL =
-  "https://sentry.io/events/dex/?promo_name=docs-banner";
+  "https://sentry.io/resources/building-an-operational-dashboard-using-error-and-performance-data/?promo_name=docs-banner";
 const BANNER_LINK_TEXT = "Register here.";
 const OPTIONAL_BANNER_IMAGE = null;
 
