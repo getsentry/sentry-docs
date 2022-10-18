6424eb4b911fc5826b7010d17832f35d3a4a62b8
Neelesh Ranjan Jha <57111920+Neeleshrj@users.noreply.github.com>
2022-10-18 21:20:50 +0530

    added navbar to layout, hide when display sm (#5650)

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Nav } from "react-bootstrap";
 
 import Breadcrumbs from "./breadcrumbs";
 import Header from "./header";
 import Sidebar from "./sidebar";
 import Navbar from "./navbar";
+import SmartLink from "./smartLink";
+import NavbarPlatformDropdown from "./navbarPlatformDropdown";
+import { getSandboxURL, SandboxOnly } from "./sandboxLink";
 
 import "~src/css/screen.scss";
 
@@ -46,6 +50,45 @@ export default ({
               {sidebar ? sidebar : <Sidebar />}
             </div>
           </div>
+        </div>
+        <div className="d-sm-none d-block" id="navbar-menu">
+          <Nav className="justify-content-center" style={{ flex: 1 }}>
+            <NavbarPlatformDropdown />
+            <Nav.Item>
+              <SmartLink className="nav-link" to="/api/">
+                API
+              </SmartLink>
+            </Nav.Item>
+            <SandboxOnly>
+              <Nav.Item>
+                <Nav.Link
+                  className="text-primary"
+                  href={getSandboxURL().toString()}
+                  target="_blank"
+                >
+                  Demo
+                </Nav.Link>
+              </Nav.Item>
+            </SandboxOnly>
+            <Nav.Item>
+              <Nav.Link href="https://sentry.io/">
+                Sign In
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 16 16"
+                  className="bi bi-arrow-right-short"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
+                  />
+                </svg>
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
         </div>
       </div>
 
