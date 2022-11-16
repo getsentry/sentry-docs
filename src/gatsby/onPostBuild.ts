9d8058bfe3ed6bf66194df9744a311512e2517ba
Tony Xiao <Zylphrex@gmail.com>
2022-11-16 09:11:55 -0500

    Revert "feat(profiling): add wizard onboarding docs (#5778)"
    This reverts commit 37677476a3f6cfd4fabb973e00b05bb5b598c207.

--- src/gatsby/onPostBuild.ts
+++ src/gatsby/onPostBuild.ts
@@ -73,25 +73,18 @@ export default async ({ graphql }) => {
 };
 
 const parsePathSlug = (slug: string) => {
-  if (
-    slug.includes("/performance-onboarding/") ||
-    slug.includes("/replay-onboarding/") ||
-    slug.includes("/profiling-onboarding/")
-  ) {
+  if (slug.includes("/performance-onboarding/") || slug.includes("/replay-onboarding/")) {
     const pathMatch = slug.match(
-      /^\/(?<platform>[^/]+)\/(?<product>performance|replay|profiling)-onboarding\/(?<sub_platform>[^/]+)\/(?<step>[^/]+)\/$/
+      /^\/(?<platform>[^/]+)\/(?<product>performance|replay)-onboarding\/(?<sub_platform>[^/]+)\/(?<step>[^/]+)\/$/
     );
 
-    if (!pathMatch) {
-      throw new Error(`Unable to parse onboarding path from slug: ${slug}`);
+    if(!pathMatch) {
+      throw new Error(`Unable to parse performance onboarding from slug: ${slug}`);
     }
 
     const { platform, product, sub_platform } = pathMatch.groups;
     const step = String(pathMatch.groups.step).replace(/\./g, "-");
-    const sub =
-      platform === sub_platform
-        ? `${product}-onboarding-${step}`
-        : `${sub_platform}-${product}-onboarding-${step}`;
+    const sub = platform === sub_platform ? `${product}-onboarding-${step}` : `${sub_platform}-${product}-onboarding-${step}`;
 
     return {
       platform,
@@ -123,7 +116,7 @@ const writeJson = async (
     if (!indexJson[main]) indexJson[main] = {};
     if (!node.frontmatter.doc_link) {
       // Skip invalid files
-      return;
+      return
     }
     const key = sub ? `${main}.${sub}` : `${main}`;
     const data = {
