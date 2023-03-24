5a959c7f8b154ab5afda450ae3f1a1cd0e7d241c
Steven Eubank <47563310+smeubank@users.noreply.github.com>
2023-03-24 13:27:11 +0100

    Update banner.tsx
    found the better text on another banner in the product from past AMAs

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -7,7 +7,7 @@ import React, { useState, useEffect } from "react";
 //
 const SHOW_BANNER = true;
 const BANNER_TEXT =
-  "Livestream AMA: Achieving code reliability across the release cycle.";
+  "AMA: Chat with the team bringing Codecov’s code coverage to Sentry stack traces so you can fill gaps in test coverage and avoid regressions.";
 const BANNER_LINK_URL =
   "https://sentry.io/resources/ama-code-reliability-across-release/";
 const BANNER_LINK_TEXT = "Register here.";
