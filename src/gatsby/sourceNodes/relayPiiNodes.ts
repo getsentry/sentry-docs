8bfa7156671e163c3f6a0b17ca2edf280f0e1b89
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-04-25 20:08:24 +0200

    wip

--- src/gatsby/sourceNodes/relayPiiNodes.ts
+++ src/gatsby/sourceNodes/relayPiiNodes.ts
@@ -1,10 +1,9 @@
-import fields from '../../data/relay_pii.json';
+import fields from '../../data/relay_event_pii.json';
 
 export const piiFieldsNodes = ({ actions, createNodeId, createContentDigest }) => {
     const { createNode } = actions;
 
     fields.forEach(field => {
-        console.log('Field:', field); // Add this line to print the field
 
         const fieldNode = {
             ...field,
