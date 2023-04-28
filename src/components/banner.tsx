35804734954e9ca1af495e0ce50d00d4cc8029bc
Rahul Chhabria <rahul.chhabria@sentry.io>
2023-04-28 15:38:52 -0700

    update banner with profiling ama

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -7,10 +7,10 @@ import React, {useEffect, useState} from 'react';
 //
 const SHOW_BANNER = true;
 const BANNER_TEXT =
-  'Syntax is joining Sentry. Check out the post and listen to the tasty 600th episode to learn more and grab limited edition swag.';
+  'Join Sentry engineers as they discuss how to use Profiling to see the exact lines of code or functions causing slowdowns in your application. ';
 const BANNER_LINK_URL =
-  'https://blog.sentry.io/2023/04/12/syntax-sentry-mmxxiii/?utm_source=web&utm_medium=docs&utm_campaign=sentry+syntax&utm_term=link';
-const BANNER_LINK_TEXT = 'Read more.';
+  'https://bit.ly/profiling-ama-reg-7';
+const BANNER_LINK_TEXT = 'Sign up now.';
 const OPTIONAL_BANNER_IMAGE = null;
 
 //
