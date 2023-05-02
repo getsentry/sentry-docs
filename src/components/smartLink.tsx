3f959a79f2bcd24779c354b6981e2154a0ea64f3
Stephen Cefali <scefali@sentry.io>
2023-05-02 15:39:40 -0700

    merge from master

--- src/components/smartLink.tsx
+++ src/components/smartLink.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import qs from 'query-string';
+
+import {marketingUrlParams} from '../utils';
 
 import ExternalLink from './externalLink';
 
@@ -26,6 +29,28 @@ export default function SmartLink({
   ...props
 }: Props): JSX.Element {
   const realTo = to || href || '';
+
+  const [forcedUrl, setForcedUrl] = React.useState(realTo);
+
+  // Google Tag Manager syncs certain query parameters to all links on the page.
+  // Since Gatsby's Link is a React component, it doesn't catch these updates
+  // because they're made outside of React, so we keep track of them ourselves.
+  React.useEffect(() => {
+    const marketingParams = marketingUrlParams();
+    if (!marketingParams) {
+      return;
+    }
+
+    const linkParams = qs.parse(realTo.split('?')[1]);
+    // Merge the new params *before* the old to ensure the old ones
+    // don't get clobbered. If they're set, they should stay.
+    const newParams = {...marketingParams, ...linkParams};
+    const urlWithoutQuery = realTo.replace(/\?.*/, '');
+    if (Object.keys(newParams).length > 0) {
+      setForcedUrl(`${urlWithoutQuery}?${qs.stringify(newParams)}`);
+    }
+  }, [realTo]);
+
   if (realTo.indexOf('://') !== -1) {
     return (
       <ExternalLink href={realTo} className={className} {...props}>
