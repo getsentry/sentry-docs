068b90dd4ca81397b7b3ccacd51b2283fc00c8b6
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-05-04 11:28:57 -0700

    ref(prettier): Configure prettier to format mdx w/ defaults
    Most of our users will be using the default prettier config, so we want
    to format our codeblocks using this default

--- prettier.config.js
+++ prettier.config.js
@@ -12,4 +12,17 @@ module.exports = {
   trailingComma: 'es5',
   useTabs: false,
   arrowParens: 'avoid',
+  overrides: [
+    // For mdx files we use the default prettier configs for codeblocks,
+    // since at least for javascript, this is what most people use
+    {
+      files: ['*.md', '*.mdx'],
+      options: {
+        bracketSpacing: true,
+        printWidth: 80,
+        singleQuote: false,
+        arrowParens: 'always',
+      },
+    },
+  ],
 };
