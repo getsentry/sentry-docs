a4f16c85aa2227e72b0e160a7d48af95ed8fb8c6
Chad Whitacre <chadwhitacre@sentry.io>
2023-05-04 20:45:00 -0400

    Add a comment and satisfy flake8

--- bin/react-to-product-owners-yml-changes.py
+++ bin/react-to-product-owners-yml-changes.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import os, sys, yaml
+import os
+import sys
+import yaml
 from os.path import realpath, join, dirname
 
 # Run from project root.
@@ -19,7 +21,11 @@ tail = []
 current = head
 
 # Best to look the other way, Buck. This is just waaaay easier than trying to
-# use ruamel.yaml to preserve comments and other formatting.
+# use ruamel.yaml to preserve comments and other formatting. What this does is
+# loop through line by line, collecting everything before the Product Area
+# labels in `head` and everything after in `tail`. It "fast-forwards" past the
+# existing Product Area labels. Then we generate output for the new Product
+# Area labels, then emit the three parts (head, product_areas, tail) in order.
 
 for line in labels:
     if line == '\n':
@@ -37,7 +43,7 @@ for area in product_owners['by_area']:
         product_areas.append(f'- name: "Product Area: {area}"\n')
     else:
         product_areas.append(f"- name: 'Product Area: {area}'\n")
-    product_areas.append(f"  color: '8D5494'\n")
+    product_areas.append("  color: '8D5494'\n")
 
 product_areas += ["- name: 'Product Area: Other'\n", "  color: '8D5494'\n"]
 
