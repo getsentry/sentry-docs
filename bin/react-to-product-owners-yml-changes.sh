c2b15f73dccad57c0654db8b3495b1694db12970
Chad Whitacre <chadwhitacre@sentry.io>
2023-05-04 23:16:45 -0400

    Only add .github/labels.yml (not whole venv!) (#6863)

--- bin/react-to-product-owners-yml-changes.sh
+++ bin/react-to-product-owners-yml-changes.sh
@@ -7,10 +7,11 @@ sha=$(git -C /tmp/security-as-code rev-parse @)
 pip3 install pyyaml==6.0
 ./bin/react-to-product-owners-yml-changes.py /tmp/security-as-code/rbac/lib/product-owners.yml
 branch="getsantry/update-product-area-labels-${sha:0:8}"
+message="Sync with product-owners.yml in security-as-code@${sha:0:8}"
 git config user.email "getsantry[bot]@users.noreply.github.com"
 git config user.name "getsantry[bot]"
 git checkout -b "$branch"
-git add .
-git commit -m "Sync with product-owners.yml in security-as-code@${sha:0:8}"
+git add .github/labels.yml
+git commit -n -m "$message"
 git push --set-upstream origin "$branch"
-gh pr create --fill
+gh pr create --title "meta(routing) $message" --body="Syncing with [``product-owners.yml``](https://github.com/getsentry/security-as-code/blob/$sha/rbac/lib/product-owners.yml) ([docs](https://www.notion.so/473791bae5bf43399d46093050b77bf0))."
