e16f9f56db5e2f5aa9310774ccd627636f3942f2
Francesco Novy <francesco.novy@sentry.io>
2023-05-10 08:46:15 +0200

    build: Use Loader Script instead of `@sentry/gatsby` (#6885)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import * as Sentry from '@sentry/gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import {getCurrentTransaction} from '../utils';
 
 function NotFoundPage() {
-  const tx = Sentry.getCurrentHub().getScope().getTransaction();
+  const tx = getCurrentTransaction();
   if (tx) {
     tx.setStatus('not_found');
   }
