65be2aa45a330ebc722a115fa45c367670268e97
Chad Whitacre <chadwhitacre@sentry.io>
2023-05-16 19:03:10 -0400

    Exit cleanly if there is nothing to commit (#6970)

--- bin/react-to-product-owners-yml-changes.sh
+++ bin/react-to-product-owners-yml-changes.sh
@@ -12,6 +12,6 @@ git config user.email "getsantry[bot]@users.noreply.github.com"
 git config user.name "getsantry[bot]"
 git checkout -b "$branch"
 git add .github/labels.yml
-git commit -n -m "$message"
+git commit -n -m "$message" || exit 0
 git push --set-upstream origin "$branch"
 gh pr create --title "meta(routing) $message" --body="Syncing with [``product-owners.yml``](https://github.com/getsentry/security-as-code/blob/$sha/rbac/lib/product-owners.yml) ([docs](https://www.notion.so/473791bae5bf43399d46093050b77bf0))."
