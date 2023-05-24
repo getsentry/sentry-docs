9f1f0cd3c98f20c4011abb937a781fde00a6b7cf
elijames-codecov <88844267+elijames-codecov@users.noreply.github.com>
2023-05-24 10:21:08 -0400

    Banner Update (#7006)
    * banner update
    
    * style(lint): Auto commit lint changes
    
    * Update src/components/banner.tsx
    
    Co-authored-by: Liza Mock <liza.mock@sentry.io>
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>
    Co-authored-by: Liza Mock <liza.mock@sentry.io>

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -5,11 +5,12 @@ import React, {useEffect, useState} from 'react';
 // This is a lazy way of doing things but will work until
 // we put a more robust solution in place.
 //
-const SHOW_BANNER = false;
+const SHOW_BANNER = true;
 const BANNER_TEXT =
-  'Join us on May 25th at 10am PT to learn how to investigate errors and performance issues with Session Replay. Plus, get the latest on new features.';
-const BANNER_LINK_URL = 'https://bit.ly/session-replay-troubleshooting-7';
-const BANNER_LINK_TEXT = 'Sign up now.';
+  'Join us on May 30th at 10am PT for a 20-minute session on frontend error monitoring with Sentry.';
+const BANNER_LINK_URL =
+  'https://sentry.io/resources/livestream-ama-frontend-error-monitoring-101/?utm_medium=banner&utm_source=sentry-app&utm_campaign=frontend-error-webinar-may&utm_content=docs-banner';
+const BANNER_LINK_TEXT = 'Register now.';
 const OPTIONAL_BANNER_IMAGE = null;
 
 //
