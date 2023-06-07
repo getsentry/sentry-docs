457f789686a3328323dc6ab281dc46e4e806a751
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-06-07 17:53:34 +0200

    wip

--- src/gatsby/sourceNodes/relayPiiNodes.ts
+++ src/gatsby/sourceNodes/relayPiiNodes.ts
@@ -1,18 +1,22 @@
-import fields from '../../data/relay_event_pii.json';
+import fs from 'fs';
+import path from 'path';
 
-export const piiFieldsNodes = ({actions, createNodeId, createContentDigest}) => {
-  const {createNode} = actions;
+export const piiFieldsNodes = ({ actions, createNodeId, createContentDigest }) => {
+    const { createNode } = actions;
 
-  fields.forEach((field, index) => {
-    const fieldNode = {
-      path: field,
-      id: createNodeId(`PiiFieldPath-${index}`),
-      internal: {
-        type: 'PiiFieldPath',
-        contentDigest: createContentDigest(field),
-      },
-    };
+    const data = fs.readFileSync(path.join(__dirname, '../../data/relay_event_pii.txt'), 'utf-8');
+    const fields = data.split('\n').map(line => line.trim()).filter(Boolean);
 
-    createNode(fieldNode);
-  });
+    fields.forEach((field, index) => {
+        const fieldNode = {
+            path: field,
+            id: createNodeId(`PiiFieldPath-${index}`),
+            internal: {
+                type: 'PiiFieldPath',
+                contentDigest: createContentDigest(field),
+            },
+        };
+
+        createNode(fieldNode);
+    });
 };
