df543e50fda392253ebee86a4434f442373b8d54
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-06-15 13:51:07 +0200

    wip

--- src/components/relayPiifields.tsx
+++ src/components/relayPiifields.tsx
@@ -2,40 +2,48 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import styled from '@emotion/styled';
 
-
 const query = graphql`
   query PiiFieldsQuery {
     allPiiFieldPath {
       nodes {
         path
+        attributes {
+          additional_properties
+        }
       }
     }
   }
 `;
+
+
+
 
 const Asterisk = styled.span`
   margin-left: 5px;
 `;
 
 function PiiField({ field }) {
-  const asterisk_flag = field.endsWith('other');
+  const asterisk_flag = field.attributes.additional_properties !== null;
+
 
   return (
     <dt>
-      <code>{field}</code>
+      <code>{field.path}</code>
       {asterisk_flag ? <Asterisk>*</Asterisk> : null}
     </dt>
   );
 }
 
+
+
 export function PiiFields(): JSX.Element {
   const data = useStaticQuery(query);
-  const fields = data.allPiiFieldPath.nodes.map(node => node.path);
+  const fields = data.allPiiFieldPath.nodes;
 
   return (
     <dl>
       {fields.map(field => (
-        <PiiField key={field} field={field} />
+        <PiiField key={field.path} field={field} />
       ))}
     </dl>
   );
--- src/gatsby/sourceNodes/relayPiiNodes.ts
+++ src/gatsby/sourceNodes/relayPiiNodes.ts
@@ -1,19 +1,25 @@
 import fs from 'fs';
 import path from 'path';
 
+
+
+
 export const piiFieldsNodes = ({ actions, createNodeId, createContentDigest }) => {
     const { createNode } = actions;
 
-    const data = fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'relay_event_pii.txt'), 'utf-8');
-    const fields = data.split('\n').map(line => line.trim()).filter(Boolean);
+    const data = fs.readFileSync(path.join(__dirname, '..', '..', 'data', 'relay_event_pii.json'), 'utf-8');
+    const fields = JSON.parse(data);
 
     fields.forEach((field, index) => {
         const fieldNode = {
-            path: field,
+            // extract path and attributes from field object
+            path: field.path,
+            attributes: field.attributes,
             id: createNodeId(`PiiFieldPath-${index}`),
             internal: {
                 type: 'PiiFieldPath',
-                contentDigest: createContentDigest(field),
+                // use JSON.stringify to convert field object into a string for contentDigest
+                contentDigest: createContentDigest(JSON.stringify(field)),
             },
         };
 
