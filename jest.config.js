51f9cf6fedf4e2b2a89bf3639ebec54875325146
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-06-15 01:18:39 -0700

    chore(deps): Upgrade testing dependencies (#7167)
    - Remove react-testing-library. This package was not used and is not
       what you think it is.
    
     - Remove @react-testing-library/react since we aren't even using this
    
     - Upgrade various testing packages to their latest versions

--- jest.config.js
+++ jest.config.js
@@ -10,13 +10,14 @@ module.exports = {
     '^sentry-docs/(.*)': '<rootDir>/src/$1',
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
-    '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    // '^gatsby-page-utils/(.*)$': `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
   },
   testPathIgnorePatterns: [`node_modules`, `\\.cache`, `<rootDir>.*/public`],
   transformIgnorePatterns: [`node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`],
   globals: {
     __PATH_PREFIX__: ``,
   },
+  testEnvironment: 'jsdom',
   testEnvironmentOptions: {
     url: `http://localhost`,
   },
--- setup-test-env.js
+++ setup-test-env.js
@@ -1,9 +1,6 @@
 /* eslint-env node */
 /* eslint import/no-nodejs-modules:0 */
 
-// https://www.gatsbyjs.com/docs/unit-testing/
-import '@testing-library/jest-dom/extend-expect';
-
 global.___loader = {
   enqueue: jest.fn(),
 };
