79517eb700334c27744e8b483f5e06a0ad66d27b
Tor <tor.saebjoernsen@sentry.io>
2023-06-20 15:56:38 +0200

    doc(relay): List full path of all Event pii-fields  (#6730)
    Lists the full paths of all fields marked as pii = true under the event struct.
    
    works together with: getsentry/relay#1972
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>
    Co-authored-by: Luca Forstner <luca.forstner@sentry.io>

--- src/gatsby/sourceNodes/index.ts
+++ src/gatsby/sourceNodes/index.ts
@@ -3,9 +3,11 @@ import {sourceAwsLambdaLayerRegistryNodes} from './awsLambdLayerRegistryNodes';
 import {sourcePackageRegistryNodes} from './packageRegistryNodes';
 import {sourcePlatformNodes} from './platformNodes';
 import {relayMetricsNodes} from './relayMetricsNodes';
+import {piiFieldsNodes} from './relayPiiNodes';
 
 async function main(params) {
   relayMetricsNodes(params);
+  piiFieldsNodes(params);
   await sourcePlatformNodes(params);
   await sourcePackageRegistryNodes(params);
   await sourceAppRegistryNodes(params);
