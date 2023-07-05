228db635b764f48f2062d7fee56c4dcac0861725
Francesco Novy <francesco.novy@sentry.io>
2023-07-05 16:38:58 +0200

    handle URL for server side rendering

--- src/components/signInNote.tsx
+++ src/components/signInNote.tsx
@@ -1,9 +1,20 @@
 import React, {useContext} from 'react';
 import {useLocation} from '@reach/router';
+import {graphql, StaticQuery} from 'gatsby';
 
 import {CodeContext} from './codeContext';
 import {ExternalLink} from './externalLink';
 import {Note} from './note';
+
+const siteMetaQuery = graphql`
+  query DefaultSEOQuery {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`;
 
 export function SignInNote(): JSX.Element {
   const location = useLocation();
@@ -18,18 +29,24 @@ export function SignInNote(): JSX.Element {
   }
 
   return (
-    <Note>
-      The following code sample will let you chose your personal config from the dropdown,
-      once you're{' '}
-      <ExternalLink
-        href={`https://sentry.io/auth/login/${
-          location && location.href ? `?nextUri=${location.href}` : ''
-        }`}
-        target="_blank"
-      >
-        logged in
-      </ExternalLink>
-      .
-    </Note>
+    <StaticQuery
+      query={siteMetaQuery}
+      render={data => {
+        const url = data.site.siteMetadata.siteUrl + location.pathname;
+        return (
+          <Note>
+            The following code sample will let you chose your personal config from the
+            dropdown, once you're{' '}
+            <ExternalLink
+              href={`https://sentry.io/auth/login/?nextUri=${url}`}
+              target="_blank"
+            >
+              logged in
+            </ExternalLink>
+            .
+          </Note>
+        );
+      }}
+    />
   );
 }
