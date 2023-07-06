2126f66f4eb09d0b7dd094f14da2e029450d0019
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-07-06 14:08:41 +0200

    ref(ts): Convert gatsby-browser to tsx

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,15 +1,15 @@
-/* eslint-env node */
-/* eslint import/no-nodejs-modules:0 */
-
 import React from 'react';
+import {GatsbyBrowser} from 'gatsby';
+
 import PageContext from 'sentry-docs/components/pageContext';
 
-export const wrapPageElement = ({element, props: {pageContext}}) => (
-  <PageContext.Provider value={pageContext}>{element}</PageContext.Provider>
-);
+export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
+  element,
+  props: {pageContext},
+}) => <PageContext.Provider value={pageContext}>{element}</PageContext.Provider>;
 
 // Disable prefetching altogether so our bw is not destroyed.
 // If this turns out to hurt performance significantly, we can
 // try out https://www.npmjs.com/package/gatsby-plugin-guess-js
 // with data from the prior 1-2 weeks.
-export const disableCorePrefetching = () => true;
+export const disableCorePrefetching: GatsbyBrowser['disableCorePrefetching'] = () => true;
