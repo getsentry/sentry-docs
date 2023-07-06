698a17271cb6d0bb18d9b123ef0db98cd3908835
Stephanie Anderson <stephanie.anderson@sentry.io>
2023-07-06 10:45:10 +0200

    Add note to sign in in order to fully benefit from code samples (#7318)
    ---------
    
    Co-authored-by: Francesco Novy <francesco.novy@sentry.io>
    Co-authored-by: vivianyentran <20403606+vivianyentran@users.noreply.github.com>

---
+++ src/components/signInNote.tsx
@@ -0,0 +1,52 @@
+import React, {useContext} from 'react';
+import {useLocation} from '@reach/router';
+import {graphql, StaticQuery} from 'gatsby';
+
+import {CodeContext} from './codeContext';
+import {ExternalLink} from './externalLink';
+import {Note} from './note';
+
+const siteMetaQuery = graphql`
+  query SignInNoteQuery {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+  }
+`;
+
+export function SignInNote(): JSX.Element {
+  const location = useLocation();
+
+  const {codeKeywords} = useContext(CodeContext);
+
+  const user = codeKeywords.USER;
+
+  // This means the user is signed in
+  if (user) {
+    return null;
+  }
+
+  return (
+    <StaticQuery
+      query={siteMetaQuery}
+      render={data => {
+        const url = data.site.siteMetadata.siteUrl + location.pathname;
+        return (
+          <Note>
+            The following code sample will let you choose your personal config from the
+            dropdown, once you're{' '}
+            <ExternalLink
+              href={`https://sentry.io/auth/login/?next=${url}`}
+              target="_blank"
+            >
+              logged in
+            </ExternalLink>
+            .
+          </Note>
+        );
+      }}
+    />
+  );
+}
