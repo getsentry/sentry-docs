b65818061aa71c9d264c382b2d8ed71285ec2a4f
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-07-06 14:31:37 +0200

    ref(ts): Convert gatsby-node to ts

---
+++ gatsby-node.ts
@@ -0,0 +1,6 @@
+export {default as onCreateNode} from './src/gatsby/onCreateNode';
+export {default as onCreateWebpackConfig} from './src/gatsby/onCreateWebpackConfig';
+export {default as onPostBuild} from './src/gatsby/onPostBuild';
+export {default as createPages} from './src/gatsby/createPages';
+export {default as createSchemaCustomization} from './src/gatsby/createSchemaCustomization';
+export {default as sourceNodes} from './src/gatsby/sourceNodes';
