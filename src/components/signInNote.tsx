2122752ef02152bdd977f799db3d11555185b407
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-07-10 15:04:31 -0700

    ref(gatsby): Switch to useStaticQuery (#7391)
    StaticQuery will be removed in gatsby 6

--- src/components/signInNote.tsx
+++ src/components/signInNote.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useLocation} from '@reach/router';
-import {graphql, StaticQuery} from 'gatsby';
+import {graphql, useStaticQuery} from 'gatsby';
 
 import {ExternalLink} from './externalLink';
 import {Note} from './note';
@@ -18,25 +18,20 @@ const siteMetaQuery = graphql`
 
 export function SignInNote() {
   const location = useLocation();
+  const data = useStaticQuery(siteMetaQuery);
+
+  const url = data.site.siteMetadata.siteUrl + location.pathname;
 
   return (
     <SignedInCheck isUserAuthenticated={false}>
-      <StaticQuery
-        query={siteMetaQuery}
-        render={data => {
-          const url = data.site.siteMetadata.siteUrl + location.pathname;
-          return (
-            <Note>
-              The following code sample will let you choose your personal config from the
-              dropdown, once you're{' '}
-              <ExternalLink href={`https://sentry.io/auth/login/?next=${url}`}>
-                logged in
-              </ExternalLink>
-              .
-            </Note>
-          );
-        }}
-      />
+      <Note>
+        The following code sample will let you choose your personal config from the
+        dropdown, once you're{' '}
+        <ExternalLink href={`https://sentry.io/auth/login/?next=${url}`}>
+          logged in
+        </ExternalLink>
+        .
+      </Note>
     </SignedInCheck>
   );
 }
