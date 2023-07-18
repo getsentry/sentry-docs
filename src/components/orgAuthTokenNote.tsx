8209a854df4350a2a228214b9968ce790a580609
Francesco Novy <francesco.novy@sentry.io>
2023-07-18 10:24:40 +0200

    show note about token creation when signed in

--- src/components/orgAuthTokenNote.tsx
+++ src/components/orgAuthTokenNote.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import {useLocation} from '@reach/router';
 import {graphql, useStaticQuery} from 'gatsby';
 
@@ -23,20 +23,27 @@ export function OrgAuthTokenNote() {
   const url = data.site.siteMetadata.siteUrl + location.pathname;
 
   return (
-    <SignedInCheck isUserAuthenticated={false}>
-      <Note>
-        {' '}
-        You can{' '}
-        <ExternalLink href="https://sentry.io/settings/auth-tokens/" target="_blank">
-          manually create an Auth Token
-        </ExternalLink>{' '}
-        or{' '}
-        <ExternalLink href={`https://sentry.io/auth/login/?next=${url}`}>
-          sign in
-        </ExternalLink>{' '}
-        to create a token directly from the docs.
-      </Note>
-      );
-    </SignedInCheck>
+    <Fragment>
+      <SignedInCheck isUserAuthenticated={false}>
+        <Note>
+          You can{' '}
+          <ExternalLink href="https://sentry.io/settings/auth-tokens/" target="_blank">
+            manually create an Auth Token
+          </ExternalLink>{' '}
+          or{' '}
+          <ExternalLink href={`https://sentry.io/auth/login/?next=${url}`}>
+            sign in
+          </ExternalLink>{' '}
+          to create a token directly from the docs.
+        </Note>
+      </SignedInCheck>
+
+      <SignedInCheck isUserAuthenticated>
+        <Note>
+          A created token will only be visible once right after creation - make sure to
+          copy it!
+        </Note>
+      </SignedInCheck>
+    </Fragment>
   );
 }
