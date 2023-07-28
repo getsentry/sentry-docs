9b2142ab2a5c070d772015d0f0cce582f9506bf3
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-07-28 16:36:42 -0700

    style(js): Fix comment in usePlatform

--- src/components/hooks/usePlatform.tsx
+++ src/components/hooks/usePlatform.tsx
@@ -173,7 +173,7 @@ export const usePlatformList = (): Platform[] => {
 
 /**
  * Return the active platform or guide.
-
+ *
  * @param value platform key in format of `platformName[.guideName]`
  */
 export const getPlatform = (key: string): Platform | Guide | null => {
