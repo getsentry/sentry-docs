31656361667c5ec352e5f9dd5a9e497770d8d8d2
Arthur Knaus <arthur.knaus@sentry.io>
2023-08-21 12:16:49 +0200

    Add initial dialog

---
+++ src/components/feedbackButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Button = styled.button`
+  position: fixed;
+  right: 0px;
+  bottom: 50%;
+  transform: translate(25%, 50%) rotate(-90deg);
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  color: #333;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 600;
+  padding: 6px 16px;
+  text-align: center;
+  text-decoration: none;
+  z-index: 9000;
+`;
+
+export function FeebdackButton(props) {
+  return <Button {...props}>Feedback</Button>;
+}
