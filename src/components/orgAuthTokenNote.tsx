21dc039baf33eb085851ac96160528f19662f66b
Francesco Novy <francesco.novy@sentry.io>
2023-08-30 08:26:38 +0200

    feat(org-tokens): Update link & docs for org token creation (#7703)
    ---------
    
    Co-authored-by: ArthurKnaus <knauar@gmail.com>

--- src/components/orgAuthTokenNote.tsx
+++ src/components/orgAuthTokenNote.tsx
@@ -1,8 +1,9 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useContext} from 'react';
 import {useLocation} from '@reach/router';
 import {graphql, useStaticQuery} from 'gatsby';
 
 import {Alert} from './alert';
+import {CodeContext} from './codeContext';
 import {ExternalLink} from './externalLink';
 import {Note} from './note';
 import {SignedInCheck} from './signedInCheck';
@@ -23,12 +24,14 @@ export function OrgAuthTokenNote() {
 
   const url = data.site.siteMetadata.siteUrl + location.pathname;
 
+  const orgAuthTokenUrl = useOrgAuthTokenUrl();
+
   return (
     <Fragment>
       <SignedInCheck isUserAuthenticated={false}>
         <Note>
           You can{' '}
-          <ExternalLink href="https://sentry.io/settings/auth-tokens/" target="_blank">
+          <ExternalLink href={orgAuthTokenUrl} target="_blank">
             manually create an Auth Token
           </ExternalLink>{' '}
           or{' '}
@@ -41,10 +44,32 @@ export function OrgAuthTokenNote() {
 
       <SignedInCheck isUserAuthenticated>
         <Alert level="warning">
-          A created token will only be visible once right after creation - make sure to
-          copy it!
+          You can{' '}
+          <ExternalLink href={orgAuthTokenUrl} target="_blank">
+            manually create an Auth Token
+          </ExternalLink>{' '}
+          or create a token directly from the docs. A created token will only be visible
+          once right after creation - make sure to copy it!
         </Alert>
       </SignedInCheck>
     </Fragment>
   );
+}
+
+export function useOrgAuthTokenUrl() {
+  const {codeKeywords, sharedKeywordSelection} = useContext(CodeContext);
+  const [sharedSelection] = sharedKeywordSelection;
+
+  // When not signed in, we use a redirect URL that uses the last org the user visited
+  if (!codeKeywords.USER) {
+    return 'https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/';
+  }
+
+  const choices = codeKeywords?.PROJECT;
+  const currentSelectionIdx = sharedSelection.PROJECT ?? 0;
+  const currentSelection = choices[currentSelectionIdx];
+
+  const org = currentSelection.ORG_SLUG;
+
+  return `https://sentry.io/settings/${org}/auth-tokens/`;
 }
