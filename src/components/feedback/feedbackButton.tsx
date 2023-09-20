48bc609c4fe2ca236cfc5bba6ca79f3d14e0ca64
Billy Vong <billyvg@users.noreply.github.com>
2023-09-20 16:58:06 -0400

    feat(feedback): New feedback button + form design
    New design based on Jesse mocks

--- src/components/feedback/feedbackButton.tsx
+++ src/components/feedback/feedbackButton.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+import feedbackIcon from '../../logos/feedback.svg';
+
 const Button = styled.button`
   position: fixed;
-  right: 0px;
-  bottom: 50%;
-  transform: translate(25%, 50%) rotate(-90deg);
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  color: #231c3d;
+  right: 1rem;
+  bottom: 1rem;
+
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  border: 1.5px solid rgba(41, 35, 47, 0.13);
+  border-radius: 12px;
+  background: #fff;
+  box-shadow: 0px 4px 24px 0px rgba(43, 34, 51, 0.12);
   cursor: pointer;
   font-size: 14px;
   font-weight: 600;
-  padding: 6px 16px;
-  text-align: center;
+  padding: 12px 16px;
   text-decoration: none;
   z-index: 9000;
+
   &:hover {
-    background-color: #eee;
-  }
-  &:focus-visible {
-    outline: 1px solid #79628c;
     background-color: #eee;
   }
 `;
 
+const ButtonText = styled.span`
+  @media (max-width: 576px) {
+    display: none;
+  }
+`;
+
 export function FeedbackButton(props) {
-  return <Button {...props}>Feedback</Button>;
+  return (
+    <Button {...props}>
+      <img src={feedbackIcon} alt="Report a bug icon" />
+      <ButtonText>Report a Bug</ButtonText>
+    </Button>
+  );
 }
