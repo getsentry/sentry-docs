b67c8877efafa095a66e9e77e345412f9557e054
Natik Gadzhi <natik@respawn.io>
2023-11-02 12:43:46 -0700

    Unit tests for CodeBlock

--- src/components/__tests__/codeBlock.test.js
+++ src/components/__tests__/codeBlock.test.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import {create} from 'react-test-renderer';
 
+import { CodeBlock } from '../codeBlock';
 import {_setCachedCodeKeywords, CodeContextProvider, DEFAULTS} from '../codeContext';
 import {makeKeywordsClickable} from '../codeKeywords';
 
 function CodeWrapper({children, ...props}) {
   return <code {...props}>{children ? makeKeywordsClickable(children) : children}</code>;
 }
+
+// Tests that CodeBlock renders SignInNote when necessary.
+describe('CodeBlock', () => {
+  beforeEach(() => {
+    _setCachedCodeKeywords(DEFAULTS);
+  });
+
+  it('renders a SignInNote above the code block if the code has a placeholder', () => {
+    const tree = create(
+      <CodeBlock filename="test" language="javascript">
+        process.env.MY_ENV = ___ORG_SLUG___
+      </CodeBlock>
+    );
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders without SignInNote if the code does not contain keyword settings', () => {
+    <CodeBlock filename="test" language="javascript" />;
+  });
+});
 
 describe('CodeWrapper', () => {
   beforeEach(() => {
