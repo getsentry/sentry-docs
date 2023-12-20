f941ad171ea6a25ec0ca74d8975176f90d5c2f29
Matt Quinn <matt.quinn@sentry.io>
2023-12-20 18:38:57 -0500

    implement GuideGrid

--- src/components/guideGrid.tsx
+++ src/components/guideGrid.tsx
@@ -1,4 +1,9 @@
-import { Note } from "./note";
+import React, {Fragment} from 'react';
+import {PlatformIcon} from 'platformicons';
+
+import {SmartLink} from './smartLink';
+import { serverContext } from 'sentry-docs/serverContext';
+import { getCurrentPlatformOrGuide, getPlatform } from 'sentry-docs/docTree';
 
 type Props = {
   className?: string;
@@ -6,5 +11,43 @@ type Props = {
 };
 
 export function GuideGrid({platform, className}: Props) {
-  return <Note>The `GuideGrid` component is under construction.</Note>;
+  const { rootNode, path } = serverContext();
+  if (!rootNode) {
+    return null;
+  }
+
+  const currentPlatform = platform ? getPlatform(rootNode, platform) : getCurrentPlatformOrGuide(rootNode, path);
+  if (!currentPlatform) {
+    return null;
+  }
+
+  if (currentPlatform.type === 'guide') {
+    return null;
+  }
+
+  if (currentPlatform.guides.length === 0) {
+    return null;
+  }
+
+  return (
+    <Fragment>
+      <div className="doc-toc-title">
+        <h6>Related Guides</h6>
+      </div>
+      <ul className={className}>
+        {currentPlatform.guides.map(guide => (
+          <li key={guide.key}>
+            <SmartLink to={guide.url}>
+              <PlatformIcon
+                platform={guide.icon ?? guide.key}
+                style={{marginRight: '0.5rem', border: 0, boxShadow: 'none'}}
+                format="sm"
+              />
+              <h4 style={{display: 'inline-block'}}>{guide.title}</h4>
+            </SmartLink>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
 }
