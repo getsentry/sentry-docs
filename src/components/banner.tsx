e6da7ff826bf2567893c9f82151e7468ab1dd9ab
Eli Lennox <eli.lennox@sentry.io>
2024-01-31 13:04:11 -0500

    swap banner

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -7,9 +7,9 @@ import React, {useEffect, useState} from 'react';
 //
 const SHOW_BANNER = true;
 const BANNER_TEXT =
-  'Join our live workshop: Identify, Trace, and Fix Endpoint Regression Issues on Jan. 31st.';
+  'Join us for our live product tour on February 22nd.';
 const BANNER_LINK_URL =
-  'https://sentry.io/resources/identify-trace-fix-endpoint-regression-issues-workshop/?promo_name=from-docs';
+  'https://sentry.io/resources/intro-to-sentry-live-product-tour-2024/?promo_name=from-docs';
 const BANNER_LINK_TEXT = 'Sign Up';
 const OPTIONAL_BANNER_IMAGE = null;
 
