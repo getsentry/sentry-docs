c5bb102c434815b4cccac42a72e688572a4c2904
Matt Quinn <matt.quinn@sentry.io>
2024-01-31 11:22:42 -0500

    port over plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -8,6 +9,12 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script
+        defer
+        data-domain="docs.sentry.io,rollup.sentry.io"
+        data-api="https://plausible.io/api/event"
+        src="https://plausible.io/js/script.tagged-events.js"
+      />
     </html>
   );
 }
