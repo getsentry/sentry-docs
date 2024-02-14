b4ff6dc08263669df59f5041c955c808e51f82cc
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-02-14 11:42:22 +0100

    fix: Mobile view

--- app/changelog/layout.tsx
+++ app/changelog/layout.tsx
@@ -4,29 +4,14 @@ import 'prism-sentry/index.css';
 import {ReactNode} from 'react';
 import {Theme} from '@radix-ui/themes';
 import type {Metadata} from 'next';
-import localFont from 'next/font/local';
+import {Rubik} from 'next/font/google';
 
 import {Navbar} from 'sentry-docs/components/changelog/navbar';
 
-const rubik = localFont({
-  src: [
-    {
-      path: '../../src/fonts/rubik-regular.woff2',
-      weight: 'normal',
-      style: 'normal',
-    },
-    {
-      path: '../../src/fonts/rubik-medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../../src/fonts/rubik-italic.woff2',
-      weight: 'normal',
-      style: 'italic',
-    },
-  ],
-  variable: '--font-rubik',
+const rubik = Rubik({
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
@@ -36,7 +21,7 @@ export const metadata: Metadata = {
 export default function ChangelogLayout({children}: {children: ReactNode}) {
   return (
     <Theme accentColor="violet" grayColor="sand" radius="large" scaling="95%">
-      <div className={`${rubik.variable} font-sans`}>
+      <div className={`${rubik.className} font-sans`}>
         <Navbar />
         <div className="bg-gray-100">{children}</div>
         <div className="w-full mx-auto h-16 relative bg-darkPurple">
