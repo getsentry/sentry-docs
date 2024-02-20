4f07066abdfd1a3d6d57379a94b491fc7dad6a77
Matt Quinn <matt.quinn@sentry.io>
2024-02-20 15:31:50 -0500

    remove modtimes

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-
 import type {MetadataRoute} from 'next';
 
 import {getDocsFrontMatter} from 'sentry-docs/mdx';
@@ -9,13 +6,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const docs = await getDocsFrontMatter();
   const sitemapEntries = docs.map(doc => ({
     url: `https://docs.sentry.io/${doc.slug}`,
-    lastModified: doc.lastModified,
   }));
   sitemapEntries.unshift({
     url: 'https://docs.sentry.io/',
-    lastModified: fs.statSync(
-      path.join(process.cwd(), 'src', 'components', 'homeClient.tsx')
-    ).mtime,
   });
   return sitemapEntries;
 }
--- src/mdx.ts
+++ src/mdx.ts
@@ -100,7 +100,6 @@ export function getAllFilesFrontMatter(folder: string = 'docs'): FrontMatter[] {
         ...frontmatter,
         slug: formatSlug(fileName),
         sourcePath: path.join(folder, fileName),
-        lastModified: fs.statSync(file).mtime,
       });
     }
   });
@@ -134,7 +133,6 @@ export function getAllFilesFrontMatter(folder: string = 'docs'): FrontMatter[] {
     const commonFiles = commonFileNames.map(commonFileName => {
       const source = fs.readFileSync(commonFileName, 'utf8');
       const {data: frontmatter} = matter(source);
-      frontmatter.lastModified = fs.statSync(commonFileName).mtime;
       return {commonFileName, frontmatter};
     });
 
