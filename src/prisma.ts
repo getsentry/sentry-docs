c01d4882521f4c46bb8189d21f4cf2ffed0370cc
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-02-25 11:05:30 +0100

    Revert "Revert "Add Changelog Maintence (#9236)" (#9237)"
    This reverts commit 8954106e95f19a92d3bbd3d4c3244ba9c538638e.

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,16 +1,9 @@
-// using the code from https://github.com/vercel/examples/blob/main/storage/postgres-prisma/lib/prisma.ts
-// eventho it's basically the same as it was before
 import {PrismaClient} from '@prisma/client';
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+const globalForPrisma = globalThis as unknown as {prisma: PrismaClient};
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV === 'development') {
-  global.prisma = prisma;
-}
-
-export default prisma;
