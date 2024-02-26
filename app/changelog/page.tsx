643a0323fd40e5f3b30bb120faefb213bf397a0a
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-02-26 14:20:28 +0100

    Improve cache busting (#9247)

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -1,19 +1,21 @@
 import {Fragment} from 'react';
 import * as Sentry from '@sentry/nextjs';
+import {GET as changelogsEndpoint} from 'app/changelog/api/route';
 import type {Metadata} from 'next';
 
 import List from 'sentry-docs/components/changelog/list';
 
 import Header from './header';
 
-// export const dynamic = 'force-dynamic';
-export const revalidate = 0;
-export const fetchCache = 'force-cache';
-
 const getChangelogs = async () => {
+  if (process.env.CI) {
+    // during CI, we invoke the API directly since the endpoint doesn't exist yet
+    return (await changelogsEndpoint()).json();
+  }
   const result = await fetch(
     `${process.env.BASE_URL || `https://${process.env.VERCEL_URL}` || 'https://localhost:3000'}/changelog/api`,
     {
+      cache: 'force-cache',
       next: {tags: ['changelogs']},
     }
   );
