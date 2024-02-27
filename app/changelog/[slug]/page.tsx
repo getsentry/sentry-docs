51f11b0763e0af2a20084a6d9f952d9ac59d2688
Daniel Griesser <daniel.griesser.86@gmail.com>
2024-02-27 20:31:44 +0100

    Move auth path top level (#9258)
    * Move auth path top level
    
    * fix broken paths
    
    * Fix paths

--- app/changelog/[slug]/page.tsx
+++ app/changelog/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import {Fragment, Suspense} from 'react';
 import {type Category, type Changelog} from '@prisma/client';
 import * as Sentry from '@sentry/nextjs';
-import {GET as sessionHandler} from 'app/changelog/api/auth/[...nextauth]/route';
+import {GET as sessionHandler} from 'app/api/auth/[...nextauth]/route';
 import type {Metadata, ResolvingMetadata} from 'next';
 import {unstable_cache} from 'next/cache';
 import Link from 'next/link';
