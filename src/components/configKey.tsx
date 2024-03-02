e1516d5e34b0d7b0d259f0704ea7d42c405505f7
Abdellah Hariti <haritiabdellah@gmail.com>
2024-03-02 14:42:48 +0100

    fix: wrap headers in links and adjust header icons styles

--- src/components/configKey.tsx
+++ src/components/configKey.tsx
@@ -30,19 +30,21 @@ export function ConfigKey({
       platform={platform}
     >
       <h3 id={name}>
-        <a
-          href={`#${name}`}
-          className="anchor before"
-          aria-label="common options permalink"
-        >
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <a href={`#${name}`} aria-label="common options permalink">
+          <svg
+            className="anchor before"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+          >
             <path
               d="M10.879 6.05L15 1.93A5.001 5.001 0 0 1 22.071 9l-4.121 4.121a1 1 0 0 1-1.414-1.414l4.12-4.121a3 3 0 1 0-4.242-4.243l-4.121 4.121a1 1 0 1 1-1.414-1.414zm2.242 11.9L9 22.07A5 5 0 1 1 1.929 15l4.121-4.121a1 1 0 0 1 1.414 1.414l-4.12 4.121a3 3 0 1 0 4.242 4.243l4.121-4.121a1 1 0 1 1 1.414 1.414zm-8.364-.122l13.071-13.07a1 1 0 0 1 1.415 1.414L6.172 19.242a1 1 0 1 1-1.415-1.414z"
               fill="currentColor"
             />
           </svg>
+          <PlatformIdentifier platform={platform} name={name} />
         </a>
-        <PlatformIdentifier platform={platform} name={name} />
       </h3>
 
       {children}
