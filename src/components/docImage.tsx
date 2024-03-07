d9024dd639d13d961bd271c3bb31498f670b4aa9
Abdellah Hariti <haritiabdellah@gmail.com>
2024-03-07 19:34:21 +0100

    feat: optimize images using Next Image

--- src/components/docImage.tsx
+++ src/components/docImage.tsx
@@ -1,15 +1,42 @@
+import imageSize from 'image-size';
+import Image from 'next/image';
+
 import {serverContext} from 'sentry-docs/serverContext';
 
-export default function DocImage({src, ...props}: React.HTMLProps<HTMLImageElement>) {
+export default function DocImage({
+  src,
+  ...props
+}: Omit<React.HTMLProps<HTMLImageElement>, 'ref' | 'placeholder'>) {
   const {path} = serverContext();
+  // If the image is not an absolute URL, we assume it's a relative path
+  // and we prepend the current path to it
   if (!src?.startsWith('/') && !src?.includes('://')) {
     src = `/${path.join('/')}/${src}`;
   }
+
+  // Next.js Image component only supports images from the public folder
+  // or from a remote server with properly configured domain
+  if (src.startsWith('http')) {
+    return <img src={src} {...props} />;
+  }
+
+  const {width, height} = imageSize(`${process.cwd()}/public${src}`);
+
   return (
-    <span style={{display: 'block', textAlign: 'center'}}>
+    <div style={{textAlign: 'center'}}>
       <a href={src} target="_blank" rel="noreferrer">
-        <img src={src} {...props} />
+        <Image
+          {...props}
+          src={src}
+          width={width}
+          height={height}
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+          alt={props.alt ?? ''}
+        />
       </a>
-    </span>
+    </div>
   );
 }
