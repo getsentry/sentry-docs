4f64b4e89281d96d00b0b3e8fa8f4d458062a7eb
Abdellah Hariti <haritiabdellah@gmail.com>
2024-03-13 14:20:16 +0000

    feat: add support for `next_steps` array in mdx frontmatter

--- src/components/pageGrid.tsx
+++ src/components/pageGrid.tsx
@@ -1,7 +1,10 @@
+import path from 'path';
+
 import Link from 'next/link';
 
-import {nodeForPath} from 'sentry-docs/docTree';
+import {DocNode, nodeForPath} from 'sentry-docs/docTree';
 import {serverContext} from 'sentry-docs/serverContext';
+import {isTruthy} from 'sentry-docs/utils';
 
 type Props = {
   nextPages: boolean;
@@ -14,21 +17,27 @@ type Props = {
 };
 
 export function PageGrid({header}: Props) {
-  const {rootNode, path} = serverContext();
+  const {rootNode, path: nodePath} = serverContext();
   if (!rootNode) {
     return null;
   }
 
-  const parentNode = nodeForPath(rootNode, path);
+  const parentNode = nodeForPath(rootNode, nodePath);
   if (!parentNode || parentNode.children.length === 0) {
     return null;
   }
+
+  const children: DocNode[] = parentNode.frontmatter.next_steps?.length
+    ? (parentNode.frontmatter.next_steps as string[])
+        .map(p => nodeForPath(rootNode, path.join(parentNode.path, p)))
+        .filter(isTruthy) ?? []
+    : parentNode.children;
 
   return (
     <nav>
       {header && <h2>{header}</h2>}
       <ul>
-        {parentNode.children
+        {children
           /* NOTE: temp fix while we figure out the reason why some nodes have empty front matter */
           .filter(c => c.frontmatter.title)
           .map(n => (
