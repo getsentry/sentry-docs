626401302fe80eb54c2dc097bfac45883063a1ce
Matt Quinn <matt.quinn@sentry.io>
2024-03-13 11:46:19 -0400

    fix some broken sitemap.xml paths
    `getDocsFrontMatter` returns an extra `/index` at the end of some paths, which
    breaks the sitemap. This is already handled by its only other caller, in
    `app/[[..path]]/page.tsx`.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -4,9 +4,18 @@ import {getDocsFrontMatter} from 'sentry-docs/mdx';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const docs = await getDocsFrontMatter();
-  const sitemapEntries = docs.map(doc => ({
-    url: `https://docs.sentry.io/${doc.slug}`,
-  }));
+
+  const sitemapEntries = docs.map(doc => {
+    let slug = doc.slug;
+    const slugParts = slug.split('/');
+    if (slugParts[slugParts.length - 1] === 'index') {
+      slug = slugParts.slice(0, slugParts.length - 1).join('/');
+    }
+    if (!slug.endsWith('/')) {
+      slug += '/';
+    }
+    return {url: `https://docs.sentry.io/${slug}`};
+  });
   sitemapEntries.unshift({
     url: 'https://docs.sentry.io/',
   });
