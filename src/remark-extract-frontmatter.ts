07707c99068a02228f4dfdcabf8a7fae76589644
Abdellah Hariti <haritiabdellah@gmail.com>
2024-03-20 13:02:57 +0000

    move remark-extract-frontmatter plugin to TS

--- src/remark-extract-frontmatter.ts
+++ src/remark-extract-frontmatter.ts
@@ -1,10 +1,13 @@
 import {load} from 'js-yaml';
+import type {Root} from 'mdast';
+import type {Plugin} from 'unified';
 import {visit} from 'unist-util-visit';
 
-export default function extractFrontmatter() {
+const extractFrontmatter: Plugin<void[], Root> = function () {
   return (tree, file) => {
     visit(tree, 'yaml', node => {
       file.data.frontmatter = load(node.value);
     });
   };
-}
+};
+export default extractFrontmatter;
