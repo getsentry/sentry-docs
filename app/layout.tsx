2fce46de1bc82273ae280c70cb39f5eb1a5ca7a5
Abdellah Hariti <haritiabdellah@gmail.com>
2024-03-25 23:46:23 +0000

    move Rubik font declaration to the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
 import './globals.css';
 
 import type {Metadata} from 'next';
+import {Rubik} from 'next/font/google';
 import Script from 'next/script';
+
+const rubik = Rubik({
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  variable: '--font-rubik',
+});
 
 export const metadata: Metadata = {
   title: 'Home',
@@ -13,7 +21,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>{children}</body>
+      <body className={`${rubik.variable} text-darkPurple`}>{children}</body>
       <Script
         defer
         data-domain="docs.sentry.io,rollup.sentry.io"
