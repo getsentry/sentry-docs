e970faa3402cd19cacc8ba70245ef6ae649dc51f
Luca Forstner <luca.forstner@sentry.io>
2024-04-08 17:17:10 +0200

    Update src/instrumentation.ts
    Co-authored-by: Daniel Griesser <daniel.griesser.86@gmail.com>

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nextjs';
 export function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     Sentry.init({
-      dsn: 'https://ad63ba38287245f2803dc220be959636@o1.ingest.sentry.io/1267915',
+      dsn: process.env.SENTRY_DSN,
       tracesSampleRate: 1,
       debug: false,
       environment: process.env.NODE_ENV === 'development' ? 'development' : undefined,
