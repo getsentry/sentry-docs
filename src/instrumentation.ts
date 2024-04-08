16d0f0876baddb835b9d3377dc15e8ef71be0614
Luca Forstner <luca.forstner@sentry.io>
2024-04-08 14:03:48 +0200

    deps: Bump SDK to v8 alpha

---
+++ src/instrumentation.ts
@@ -0,0 +1,22 @@
+import * as Sentry from '@sentry/nextjs';
+
+export function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    Sentry.init({
+      dsn: 'https://ad63ba38287245f2803dc220be959636@o1.ingest.sentry.io/1267915',
+      tracesSampleRate: 1,
+      debug: false,
+      environment: process.env.NODE_ENV === 'development' ? 'development' : undefined,
+      spotlight: process.env.NODE_ENV === 'development',
+    });
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    Sentry.init({
+      dsn: 'https://ad63ba38287245f2803dc220be959636@o1.ingest.sentry.io/1267915',
+      tracesSampleRate: 1,
+      debug: false,
+      environment: process.env.NODE_ENV === 'development' ? 'development' : undefined,
+    });
+  }
+}
