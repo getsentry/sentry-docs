ce7f8562b5f45f8699cfd703dbf57fa528049b21
Abdellah Hariti <haritiabdellah@gmail.com>
2024-04-15 23:21:21 +0100

    fix platformGrid margin bottom

--- src/components/platformGridClient.tsx
+++ src/components/platformGridClient.tsx
@@ -68,6 +68,7 @@ const PlatformGrid = styled.div`
   display: grid;
   gap: 1.5rem;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  margin-bottom: 1.5rem;
 `;
 
 export function PlatformGridClient({noGuides = false, platforms}: Props) {
