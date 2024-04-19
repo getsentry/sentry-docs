b6ab38b9c243c3884a4736139acfea79402fa489
Abdellah Hariti <haritiabdellah@gmail.com>
2024-04-19 16:53:52 +0100

    feat: platformSelector guide nesting

--- src/components/platformSelector/index.tsx
+++ src/components/platformSelector/index.tsx
@@ -1,13 +1,22 @@
 'use client';
-import {startTransition, useEffect, useMemo, useRef, useState} from 'react';
+import {
+  Fragment,
+  Ref,
+  startTransition,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import {Combobox, ComboboxItem, ComboboxList, ComboboxProvider} from '@ariakit/react';
-import {CaretSortIcon, MagnifyingGlassIcon} from '@radix-ui/react-icons';
+import {CaretRightIcon, CaretSortIcon, MagnifyingGlassIcon} from '@radix-ui/react-icons';
 import * as RadixSelect from '@radix-ui/react-select';
 import {matchSorter} from 'match-sorter';
 import {useRouter} from 'next/navigation';
 
 import {PlatformIcon} from 'sentry-docs/components/platformIcon';
 import {Platform, PlatformGuide} from 'sentry-docs/types';
+import {uniqByReference} from 'sentry-docs/utils';
 
 import styles from './style.module.scss';
 
@@ -18,16 +27,31 @@ export function PlatformSelector({
   platforms: Array<Platform>;
   currentPlatform?: Platform | PlatformGuide;
 }) {
-  // append guides to each platform
   const platformsAndGuides = platforms
     .map(platform => [
       platform,
-      platform.guides.map((guide, i) => ({
+      ...platform.guides.map(guide => ({
         ...guide,
-        isLastGuide: i === platform.guides.length - 1,
+        // add a reference to the parent platform instead of its key
+        platform,
       })),
     ])
     .flat(2);
+
+  const [expandedPlatforms, setExpandedPlatforms] = useState<Set<string>>(new Set());
+
+  const onToggleExpand = (platformKey: string) => {
+    setExpandedPlatforms(prev => {
+      const expanded = new Set(prev);
+      if (expanded.has(platformKey)) {
+        expanded.delete(platformKey);
+      } else {
+        expanded.add(platformKey);
+      }
+      return expanded;
+    });
+  };
+
   const currentPlatformKey = currentPlatform?.key;
   const [open, setOpen] = useState(false);
   const [searchValue, setSearchValue] = useState('');
@@ -77,7 +101,6 @@ export function PlatformSelector({
       <ComboboxProvider
         open={open}
         setOpen={setOpen}
-        resetValueOnHide
         includesBaseElement={false}
         setValue={v => startTransition(() => setSearchValue(v))}
       >
@@ -117,34 +140,145 @@ export function PlatformSelector({
             />
           </div>
           <ComboboxList className={styles.listbox}>
-            {matches.map(platform => (
-              <RadixSelect.Item
-                key={platform.key}
-                value={platform.key}
-                asChild
-                className={styles.item}
-                data-guide={searchValue === '' && platform.type === 'guide'}
-                data-last-guide={platform.type === 'guide' && platform.isLastGuide}
-                ref={platform.key === currentPlatformKey ? activeElementRef : null}
-              >
-                <ComboboxItem>
-                  <RadixSelect.ItemText>
-                    <span className={styles['item-text']}>
-                      <PlatformIcon
-                        platform={platform.icon ?? platform.key}
-                        size={16}
-                        format="sm"
-                        className={styles['platform-icon']}
-                      />
-                      {platform.title?.replace(/(.)\.(.)/g, '$1 $2')}
-                    </span>
-                  </RadixSelect.ItemText>
-                </ComboboxItem>
-              </RadixSelect.Item>
-            ))}
+            {uniqByReference(
+              matches
+                // map guides to parent platforms
+                .map(x => (x.type === 'platform' ? x : x.platform))
+            ).map(platform => {
+              return (
+                <PlatformItem
+                  key={platform.key}
+                  platform={{
+                    ...platform,
+                    // only keep guides that are in the matches list
+                    guides: platform.guides.filter(g =>
+                      matches.some(m => m.key === g.key)
+                    ),
+                    isExpanded:
+                      // expand search results
+                      searchValue !== '' ||
+                      expandedPlatforms.has(platform.key) ||
+                      // expand current platform/parent of current guide
+                      platform.key === currentPlatformKey ||
+                      platform.guides.some(g => g.key === currentPlatformKey),
+                  }}
+                  activeElementRef={
+                    platform.key === currentPlatformKey ||
+                    platform.guides.some(g => g.key === currentPlatformKey)
+                      ? activeElementRef
+                      : null
+                  }
+                  activeItemKey={currentPlatformKey}
+                  onPlatformExpand={onToggleExpand}
+                />
+              );
+            })}
           </ComboboxList>
         </RadixSelect.Content>
       </ComboboxProvider>
     </RadixSelect.Root>
+  );
+}
+
+type PlatformItemProps = {
+  activeElementRef: Ref<HTMLDivElement>;
+  platform: Platform & {isExpanded?: boolean};
+  activeItemKey?: string;
+  onPlatformExpand?: (platformKey: string) => void;
+};
+function PlatformItem({
+  platform,
+  activeElementRef: activeItemRef,
+  activeItemKey: activeElementKey,
+  onPlatformExpand: onExpand,
+}: PlatformItemProps) {
+  return (
+    <Fragment>
+      {/* This is a hack. The Label allows us to have a clickable button inside the item without triggering its selection */}
+      <RadixSelect.Group>
+        <RadixSelect.Label className="flex">
+          <Fragment>
+            <RadixSelect.Item
+              value={platform.key}
+              asChild
+              className={styles.item}
+              data-platform-with-guides
+              ref={activeElementKey === platform.key ? activeItemRef : null}
+            >
+              <ComboboxItem>
+                <RadixSelect.ItemText>
+                  <span className={styles['item-text']}>
+                    <PlatformIcon
+                      platform={platform.icon ?? platform.key}
+                      size={16}
+                      format="sm"
+                      className={styles['platform-icon']}
+                    />
+                    {platform.title?.replace(/(.)\.(.)/g, '$1 $2')}
+                  </span>
+                </RadixSelect.ItemText>
+              </ComboboxItem>
+            </RadixSelect.Item>
+            {platform.guides.length > 0 && (
+              <button
+                className={styles['expand-button']}
+                disabled={
+                  activeElementKey === platform.key ||
+                  platform.guides.some(g => g.key === activeElementKey)
+                }
+                onClick={() => {
+                  onExpand?.(platform.key);
+                }}
+                data-expanded={platform.isExpanded}
+              >
+                <CaretRightIcon />
+              </button>
+            )}
+          </Fragment>
+        </RadixSelect.Label>
+      </RadixSelect.Group>
+      {platform.isExpanded &&
+        platform.guides
+          .map((guide, i) => ({
+            ...guide,
+            isLastGuide: i === platform.guides.length - 1,
+          }))
+          .map(guide => {
+            const _ref = guide.key === activeElementKey ? activeItemRef : null;
+            return <GuideItem key={guide.key} guide={guide} ref={_ref} />;
+          })}
+    </Fragment>
+  );
+}
+
+type GuideItemProps = {
+  guide: (PlatformGuide & {isLastGuide: boolean}) | Platform;
+  ref: Ref<HTMLDivElement>;
+};
+function GuideItem({guide, ref}: GuideItemProps) {
+  return (
+    <RadixSelect.Item
+      key={guide.key}
+      value={guide.key}
+      asChild
+      className={styles.item}
+      data-guide
+      data-last-guide={guide.type === 'guide' && guide.isLastGuide}
+      ref={ref}
+    >
+      <ComboboxItem>
+        <RadixSelect.ItemText>
+          <span className={styles['item-text']}>
+            <PlatformIcon
+              platform={guide.icon ?? guide.key}
+              size={16}
+              format="sm"
+              className={styles['platform-icon']}
+            />
+            {guide.title?.replace(/(.)\.(.)/g, '$1 $2')}
+          </span>
+        </RadixSelect.ItemText>
+      </ComboboxItem>
+    </RadixSelect.Item>
   );
 }
