4d29269b3e51ed9fa78d1619f8e2b3e738d769b0
Abdellah Hariti <haritiabdellah@gmail.com>
2024-04-20 15:37:42 +0100

    feat: nest guides under platforms on home page sdk search

--- src/components/platformFilter/index.tsx
+++ src/components/platformFilter/index.tsx
@@ -1,17 +1,21 @@
 'use client';
 import {useMemo, useState} from 'react';
+import * as Collapsible from '@radix-ui/react-collapsible';
+import {TriangleRightIcon} from '@radix-ui/react-icons';
+import classNames from 'classnames';
 import {matchSorter} from 'match-sorter';
+import Link from 'next/link';
 
-import {type Platform, PlatformGuide} from 'sentry-docs/types';
+import {type Platform} from 'sentry-docs/types';
+import {uniqByReference} from 'sentry-docs/utils';
 
 import styles from './style.module.scss';
 
 import {PlatformIcon} from '../platformIcon';
-import {SmartLink} from '../smartLink';
 
 export function PlatformFilter({platforms}: {platforms: Platform[]}) {
-  const platformsAndGuides: (Platform | PlatformGuide)[] = platforms
-    .map(p => [p, p.guides])
+  const platformsAndGuides = platforms
+    .map(p => [p, p.guides.map(g => ({...g, platform: p}))])
     .flat(2);
   const [filter, setFilter] = useState('');
 
@@ -33,35 +37,95 @@ export function PlatformFilter({platforms}: {platforms: Platform[]}) {
         </div>
         <div className="w-full flex justify-end">
           <input
-            placeholder="Filter platforms"
+            placeholder="Search SDKs"
             className={`${styles.input}`}
             value={filter}
             onChange={e => setFilter(e.target.value)}
           />
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-8 items-start">
         {matches.length > 0 &&
-          matches.map(platform => (
-            <SmartLink to={platform.url} key={platform.key}>
-              <div className="flex gap-2 shadow rounded p-2">
-                <div className="w-5 flex">
-                  <PlatformIcon
-                    size={20}
-                    platform={platform.icon ?? platform.key}
-                    format="lg"
-                  />
-                </div>
-                {platform.title}
-              </div>
-            </SmartLink>
-          ))}
+          uniqByReference(matches.map(x => (x.type === 'platform' ? x : x.platform)))
+            .map(p => {
+              return {
+                ...p,
+                guides: p.guides.filter(g => matches.some(m => m.key === g.key)),
+              };
+            })
+            .map(platform =>
+              platform.guides.length === 0 ? (
+                <Link href={platform.url} key={platform.key}>
+                  <div className={styles.StandalonePlatform}>
+                    <PlatformIcon
+                      size={20}
+                      platform={platform.icon ?? platform.key}
+                      format="lg"
+                      className={styles.PlatformIcon}
+                    />
+                    {platform.title}
+                  </div>
+                </Link>
+              ) : (
+                <PlatformWithGuides key={platform.key} platform={platform} />
+              )
+            )}
         {!matches.length && (
-          <div className="col-span-3 text-center text-gray-600">
-            No results found for query: "<code>{filter}</code>"
-          </div>
+          <div className="col-span-3 text-center text-gray-600">No results found</div>
         )}
       </div>
     </div>
+  );
+}
+
+function PlatformWithGuides({platform}: {platform: Platform}) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <Collapsible.Root
+      className={styles.CollapsibleRoot}
+      open={expanded}
+      onOpenChange={setExpanded}
+    >
+      <Collapsible.Trigger asChild className={classNames(styles.CollapsibleTrigger)}>
+        <div>
+          <Link href={platform.url} key={platform.key}>
+            <div className={styles.PlatformTitle}>
+              <PlatformIcon
+                size={20}
+                platform={platform.icon ?? platform.key}
+                format="lg"
+                className={styles.PlatformIcon}
+              />
+              {platform.title}
+            </div>
+          </Link>
+          <button className={styles.ChevronButton}>
+            <TriangleRightIcon
+              className={styles.CollapsibleChevron}
+              aria-hidden
+              width="20"
+              height="20"
+            />
+          </button>
+        </div>
+      </Collapsible.Trigger>
+      <Collapsible.Content className={classNames(styles.CollapsibleContent)}>
+        <div className={styles.CollapsibleContentText}>
+          {platform.guides.map(guide => (
+            <Link href={guide.url} key={guide.key}>
+              <div className={styles.Guide}>
+                <PlatformIcon
+                  size={20}
+                  platform={guide.icon ?? guide.key}
+                  format="lg"
+                  className={styles.PlatformIcon}
+                />
+                {guide.title}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
