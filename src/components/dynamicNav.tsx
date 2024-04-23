b2920ee524055ac62840c425155db0b15a387763
Abdellah Hariti <haritiabdellah@gmail.com>
2024-04-23 19:29:11 +0100

    Revert "refactor: different highlighting for sidebar title vs links"
    This reverts commit 3390dd13f368b2e05d213bf211e978882dae68f8.

--- src/components/dynamicNav.tsx
+++ src/components/dynamicNav.tsx
@@ -156,10 +156,9 @@ export function DynamicNav({
     parentNode && !noHeadingLink ? (
       <SmartLink
         to={`/${root}/`}
-        className={headerClassName}
+        className={`${headerClassName} ${path.join('/') === root ? 'active' : ''}`}
         activeClassName="active"
         data-sidebar-link
-        data-active-header={isActive}
       >
         <h6>{title}</h6>
       </SmartLink>
