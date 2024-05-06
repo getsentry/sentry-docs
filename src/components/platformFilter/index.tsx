3265e40ca5416cc7650cd1ed0a26d9d9a21c16b4
Abdellah Hariti <haritiabdellah@gmail.com>
2024-05-06 21:40:37 +0100

    better platformFilter search field alignment

--- src/components/platformFilter/index.tsx
+++ src/components/platformFilter/index.tsx
@@ -45,7 +45,7 @@ export function PlatformFilter({platforms}: {platforms: Platform[]}) {
   );
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-8 md:items-end">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-8 md:items-end">
         <div className="lg:col-span-2 space-y-2">
           <h2 className="text-2xl font-medium">Choose your SDK</h2>
           <p className="m-0">If you use it, we probably support it.</p>
