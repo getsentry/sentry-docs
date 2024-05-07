450f988a8f3d78c954138d47098bff9d324a9bed
Abdellah Hariti <haritiabdellah@gmail.com>
2024-05-07 13:16:22 +0100

    refactor: migrate articleFooter to CSS modules

--- src/components/changelog/articleFooter/index.tsx
+++ src/components/changelog/articleFooter/index.tsx
@@ -1,9 +1,13 @@
-import {Button} from './ui/Button';
+import {Button} from '../ui/Button';
+
+import styles from './styles.module.css';
 
 export default function ArticleFooter() {
   return (
     <div>
-      <div className="try-sentry-footer-wrapper mb-8 py-5 px-8 rounded-md flex flex-col md:flex-row items-center justify-center flex-wrap gap-2">
+      <div
+        className={`${styles['footer-wrapper']} mb-8 py-5 px-8 rounded-md flex flex-col md:flex-row items-center justify-center flex-wrap gap-2`}
+      >
         <div className="flex-1 text-white uppercase text-base">
           Your code is broken. Let's Fix it.
         </div>
