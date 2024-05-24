800b6812ffd4d744eff6b5d00e640c52a3fa1dd4
Abdellah Hariti <haritiabdellah@gmail.com>
2024-05-24 13:37:04 +0100

    sticky onboarding option buttons

--- src/components/onboarding/index.tsx
+++ src/components/onboarding/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import {QuestionMarkCircledIcon} from '@radix-ui/react-icons';
 import * as Tooltip from '@radix-ui/react-tooltip';
 import {Button, Checkbox, Theme} from '@radix-ui/themes';
@@ -120,8 +120,33 @@ export function OnboardingOptionsButtons({
     });
   }, [options]);
 
+  const buttonsRef = useRef<HTMLDivElement>(null);
+  const containerTopPx = 100;
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      function ([buttonsContainer]) {
+        setIsSticky(!buttonsContainer.isIntersecting);
+      },
+      {
+        // the 1 exptra px is important to trigger the observer
+        // https://stackoverflow.com/questions/16302483/event-to-detect-when-positionsticky-is-triggered
+        rootMargin: `-${containerTopPx + 1}px 0px 0px 0px`,
+        threshold: [1],
+      }
+    );
+
+    observer.observe(buttonsRef.current!);
+  }, []);
+
   return (
-    <div className="flex gap-4 py-5">
+    <div
+      ref={buttonsRef}
+      className={`flex gap-4 py-2 bg-white/90 sticky top-[${containerTopPx}px] z-[1000] rounded shadow-[var(--shadow-6)] transition ${
+        isSticky ? 'px-2 backdrop-blur' : ''
+      }`}
+    >
       {options.map(option => (
         <Button
           variant="surface"
