3f5108861029ef83fdd57a46a4ecb96da42fde2a
Eli Lennox <eli.lennox@sentry.io>
2024-06-26 09:09:19 -0700

    banner update

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -11,8 +11,8 @@ import styles from './banner.module.scss';
 // we put a more robust solution in place.
 //
 const SHOW_BANNER = true;
-const BANNER_TEXT = 'Join Our Live Demo: Intro to Sentry & Codecov on June 25';
-const BANNER_LINK_URL = 'https://sentry.io/resources/intro-to-sentry-and-codecov-live-demo/';
+const BANNER_TEXT = 'Behind the Code: A Conversation with Backend Experts" featuring CEOs of Laravel, Prisma, and Supabase.';
+const BANNER_LINK_URL = 'https://sentry.io/resources/behind-the-code-a-discussion-with-backend-experts';
 const BANNER_LINK_TEXT = 'RSVP';
 const OPTIONAL_BANNER_IMAGE = null;
 
