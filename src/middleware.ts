ff344c3ed620cf550bfad814933232389effe172
Liza Mock <liza.mock@sentry.io>
2024-07-08 12:54:23 -0700

    Update src/middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -62,7 +62,7 @@ type Redirect = {
 
 const REDIRECTS: Redirect[] = [
   {
-    fro: '/platforms/javascript/guides/react/configuration/integrations/trycatch/',
+    from: '/platforms/javascript/guides/react/configuration/integrations/trycatch/',
     to: '/platforms/javascript/configuration/integrations/browserapierrors/',
   },
   {
