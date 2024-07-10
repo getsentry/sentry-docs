619ec78c91e98e03ed8727d0ddc7dee4114c2167
Charly Gomez <charly.gomez@sentry.io>
2024-07-10 14:17:37 +0200

    chore: re-trigger deploy

--- next.config.js
+++ next.config.js
@@ -57,7 +57,7 @@ module.exports = withSentryConfig(module.exports, {
   // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors.
+  // Enables automatic instrumentation of Vercel Cron Monitors
   // See the following for more information:
   // https://docs.sentry.io/product/crons/
   // https://vercel.com/docs/cron-jobs
