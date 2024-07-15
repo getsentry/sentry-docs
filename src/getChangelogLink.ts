5cc010e3623e3f97ee256efeaeef185bb5bf1726
Charly Gomez <charly.gomez@sentry.io>
2024-07-15 18:17:20 +0200

    chore: add comment

--- src/getChangelogLink.ts
+++ src/getChangelogLink.ts
@@ -1,4 +1,5 @@
 import {isDeveloperDocs} from './isDeveloperDocs';
 
+// changelog app is only available on user-facing docs
 export const getChangelogLink = () =>
   isDeveloperDocs ? 'https://docs.sentry.io/changelog' : '/changelog';
