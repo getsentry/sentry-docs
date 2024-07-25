4e5cd813722228cbe3f44a9901d482a78c908b49
Luca Forstner <luca.forstner@sentry.io>
2024-07-25 12:03:56 +0200

    feat(changelog): Remove `/changelog` basepath (#10842)

--- apps/changelog/src/server/remark-component-spacing.ts
+++ apps/changelog/src/server/remark-component-spacing.ts
@@ -1,9 +1,9 @@
-import type { Root } from "mdast";
-import type { Plugin } from "unified";
-import type { Node } from "unist";
-import { visit } from "unist-util-visit";
+import type {Root} from 'mdast';
+import type {Plugin} from 'unified';
+import type {Node} from 'unist';
+import {visit} from 'unist-util-visit';
 
-const affectedComponents = ["PlatformIdentifier"];
+const affectedComponents = ['PlatformIdentifier'];
 
 const remarkComponentSpacing: Plugin<void[], Root> = function () {
   return (tree, _file) => {
@@ -15,19 +15,19 @@ const remarkComponentSpacing: Plugin<void[], Root> = function () {
           // If the component is followed by text starting with a space, turn it
           // into a no-break space character (unicode 00A0). Otherwise MDX
           // removes the space, leading to smushed text.
-          if (node.type === "text" && node.value && node.value[0] === " ") {
+          if (node.type === 'text' && node.value && node.value[0] === ' ') {
             node.value = `\u00A0${node.value.substring(1)}`;
           }
         }
         componentNode = componentNodeParent = undefined;
       } else if (
         // @ts-expect-error TODO(lforst): leftover from migration
-        node.type === "mdxJsxTextElement" &&
+        node.type === 'mdxJsxTextElement' &&
         // @ts-expect-error TODO(lforst): leftover from migration
-        affectedComponents.includes(node.name ?? "")
+        affectedComponents.includes(node.name ?? '')
       ) {
         componentNode = node;
-        // @ts-expect-error TODO(lforst): leftover from migration
+        // @ts-ignore TODO(lforst): leftover from migration
         componentNodeParent = parent;
       }
     });
