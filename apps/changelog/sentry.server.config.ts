ab11e5ce61e8ee729efe35cb898516115a9983a7
Luca Forstner <luca.forstner@sentry.io>
2024-07-25 15:08:49 +0200

    feat(changelog): Add Sentry SDK (#10824)

---
+++ apps/changelog/sentry.server.config.ts
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 1,
+  debug: false,
+  environment: process.env.NODE_ENV,
+  spotlight: process.env.NODE_ENV === 'development',
+  integrations: [Sentry.prismaIntegration()],
+});
