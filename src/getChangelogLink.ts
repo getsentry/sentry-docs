7c6ff4a7445b00569c7d6537cfa6ef6f64478665
Luca Forstner <luca.forstner@sentry.io>
2024-07-29 22:55:07 +0200

    fix

--- src/getChangelogLink.ts
+++
@@ -1,5 +0,0 @@
-import {isDeveloperDocs} from './isDeveloperDocs';
-
-// changelog app is only available on user-facing docs
-export const getChangelogLink = () =>
-  isDeveloperDocs ? 'https://sentry.io/changelog/' : '/changelog';
