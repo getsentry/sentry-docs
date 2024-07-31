94c044dfd08af73c41110f6721270b103ab0a759
abdellah hariti <haritiabdellah@gmail.com>
2024-07-31 15:22:29 +0100

    fix expandable header padding (#10927)

--- src/components/expandable.tsx
+++ src/components/expandable.tsx
@@ -36,6 +36,9 @@ const ExpandableWrapper = styled.div`
   border-left: 3px solid var(--accent-12);
   margin-bottom: 1rem;
   padding: 0.5rem 1rem;
+  h2 {
+    margin-top: 0;
+  }
 `;
 
 function slugify(str: string) {
