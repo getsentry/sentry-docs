cf881623b9efdd0fd8a663c7c56b01c01d10f97d
Luca Forstner <luca.forstner@sentry.io>
2024-08-08 12:22:50 +0200

    instrument

--- apps/changelog/src/app/changelog/page.tsx
+++ apps/changelog/src/app/changelog/page.tsx
@@ -5,29 +5,34 @@ import {serialize} from 'next-mdx-remote/serialize';
 import Header from './header';
 import {getChangelogs} from '../../server/utils';
 import {ChangelogEntry, ChangelogList} from '@/client/components/list';
+import {startSpan} from '@sentry/nextjs';
 
 export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   const changelogs = await getChangelogs();
 
-  const changelogsWithMdxSummaries = await Promise.all(
-    changelogs
-      .filter(changelog => {
-        return changelog.publishedAt !== null;
-      })
-      .map(async (changelog): Promise<ChangelogEntry> => {
-        const mdxSummary = await serialize(changelog.summary || '');
-        return {
-          id: changelog.id,
-          title: changelog.title,
-          slug: changelog.slug,
-          // Because `getChangelogs` is cached, it sometimes returns its results serialized and sometimes not. Therefore we have to deserialize the string to be able to call toUTCString().
-          publishedAt: new Date(changelog.publishedAt!).toUTCString(),
-          categories: changelog.categories,
-          mdxSummary,
-        };
-      })
+  const changelogsWithPublishedAt = changelogs.filter(changelog => {
+    return changelog.publishedAt !== null;
+  });
+
+  const changelogsWithMdxSummaries = await startSpan(
+    {name: 'serialize changelog summaries'},
+    () =>
+      Promise.all(
+        changelogsWithPublishedAt.map(async (changelog): Promise<ChangelogEntry> => {
+          const mdxSummary = await serialize(changelog.summary || '');
+          return {
+            id: changelog.id,
+            title: changelog.title,
+            slug: changelog.slug,
+            // Because `getChangelogs` is cached, it sometimes returns its results serialized and sometimes not. Therefore we have to deserialize the string to be able to call toUTCString().
+            publishedAt: new Date(changelog.publishedAt!).toUTCString(),
+            categories: changelog.categories,
+            mdxSummary,
+          };
+        })
+      )
   );
 
   return (
