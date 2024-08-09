708eeac7173ce624f4ed69bd3be47370d39290d7
Luca Forstner <luca.forstner@sentry.io>
2024-08-09 13:10:44 +0200

    ref: Refactor changelog list page (#11019)

--- apps/changelog/src/app/changelog/page.tsx
+++ apps/changelog/src/app/changelog/page.tsx
@@ -1,30 +1,44 @@
-import { Fragment } from "react";
-import type { Metadata } from "next";
-import { serialize } from "next-mdx-remote/serialize";
+import {Fragment} from 'react';
+import type {Metadata} from 'next';
+import {serialize} from 'next-mdx-remote/serialize';
 
-import Header from "./header";
-import { getChangelogs } from "../../server/utils";
-import List from "@/client/components/list";
+import Header from './header';
+import {getChangelogs} from '../../server/utils';
+import {ChangelogEntry, ChangelogList} from '@/client/components/list';
+import {startSpan} from '@sentry/nextjs';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
-export default async function ChangelogList() {
+export default async function Page() {
   const changelogs = await getChangelogs();
 
-  const changelogsWithMdxSummaries = await Promise.all(
-    changelogs.map(async (changelog) => {
-      const mdxSummary = await serialize(changelog.summary || "");
-      return {
-        ...changelog,
-        mdxSummary,
-      };
-    })
+  const changelogsWithPublishedAt = changelogs.filter(changelog => {
+    return changelog.publishedAt !== null;
+  });
+
+  const changelogsWithMdxSummaries = await startSpan(
+    {name: 'serialize changelog summaries'},
+    () =>
+      Promise.all(
+        changelogsWithPublishedAt.map(async (changelog): Promise<ChangelogEntry> => {
+          const mdxSummary = await serialize(changelog.summary || '');
+          return {
+            id: changelog.id,
+            title: changelog.title,
+            slug: changelog.slug,
+            // Because `getChangelogs` is cached, it sometimes returns its results serialized and sometimes not. Therefore we have to deserialize the string to be able to call toUTCString().
+            publishedAt: new Date(changelog.publishedAt!).toUTCString(),
+            categories: changelog.categories,
+            mdxSummary,
+          };
+        })
+      )
   );
 
   return (
     <Fragment>
       <Header />
-      <List changelogs={changelogsWithMdxSummaries} />
+      <ChangelogList changelogs={changelogsWithMdxSummaries} />
     </Fragment>
   );
 }
@@ -32,7 +46,7 @@ export default async function ChangelogList() {
 export function generateMetadata(): Metadata {
   return {
     description:
-      "Stay up to date on everything big and small, from product updates to SDK changes with the Sentry Changelog.",
+      'Stay up to date on everything big and small, from product updates to SDK changes with the Sentry Changelog.',
     alternates: {
       canonical: `https://sentry.io/changelog/`,
     },
