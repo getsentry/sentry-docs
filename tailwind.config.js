5decfd8328313041d924fd5bf05f2f2914f815d1
Abdellah Hariti <haritiabdellah@gmail.com>
2024-08-14 12:24:47 +0100

    adjust tailwind dark mode strategy

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,8 @@ import {
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: ['selector', '.dark'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       animation: {
