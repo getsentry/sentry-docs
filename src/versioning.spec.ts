304014e05e51265ce26b87710779b8e8d1898c6f
Charly Gomez <charly.gomez@sentry.io>
2024-10-01 11:40:29 +0200

    feat: highlight versioned pages in sidebar

---
+++ src/versioning.spec.ts
@@ -0,0 +1,12 @@
+import {describe, expect, test} from 'vitest';
+
+import {getUnversionedPath} from './versioning';
+
+describe('versioning', () => {
+  test('should return unversioned paths', () => {
+    expect(getUnversionedPath('/')).toBe('/');
+    expect(getUnversionedPath('/some/path')).toBe('/some/path/');
+    expect(getUnversionedPath('/some/path__v2')).toBe('/some/path/');
+    expect(getUnversionedPath('/some/path__v2/')).toBe('/some/path/');
+  });
+});
--- src/versioning.ts
+++ src/versioning.ts
@@ -1,2 +1,8 @@
 export const VERSION_INDICATOR = '__v';
+
 export const getLocalStorageVersionKey = (platform: string) => `version:${platform}`;
+
+export const getUnversionedPath = (path: string) => {
+  const unversioned = path.split(VERSION_INDICATOR)[0];
+  return unversioned[unversioned.length - 1] === '/' ? unversioned : `${unversioned}/`;
+};
