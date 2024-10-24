0729f93f1217041f3a07d9216f202cf8697588a8
Charly Gomez <charly.gomez@sentry.io>
2024-10-24 14:36:28 +0200

    add kapa script

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,15 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         data-api="https://plausible.io/api/event"
         src="https://plausible.io/js/script.tagged-events.js"
       />
+      <Script
+        async
+        src="https://widget.kapa.ai/kapa-widget.bundle.js"
+        data-website-id="ec502f3e-ef02-4b03-b345-160b3e2b6a0c"
+        data-project-name="Sentry"
+        data-project-color="#6A5FC1"
+        data-project-logo="https://docs.sentry.io/_next/static/media/sentry-logo-dark.fc8e1eeb.svg"
+        data-font-family="var(--font-rubik)"
+      />
     </html>
   );
 }
