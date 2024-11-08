24a538a1ec85c4f4ce866689deffc35ffdf239b2
Abdellah Hariti <haritiabdellah@gmail.com>
2024-11-08 17:46:52 +0100

    a better solution

--- src/build/resolveOpenAPI.ts
+++ src/build/resolveOpenAPI.ts
@@ -26,11 +26,7 @@ async function resolveOpenAPI(): Promise<DeRefedOpenAPI> {
     }
   }
   const response = await fetch(
-    `https://raw.githubusercontent.com/getsentry/sentry-api-schema/${SENTRY_API_SCHEMA_SHA}/openapi-derefed.json`,
-    {
-      // this request is not getting cached due to size limit anyway and is printing noise to the build logs
-      cache: 'no-store',
-    }
+    `https://raw.githubusercontent.com/getsentry/sentry-api-schema/${SENTRY_API_SCHEMA_SHA}/openapi-derefed.json`
   );
   return await response.json();
 }
