d7318f312f82a808778db09ca4476c202f8f8e15
Anton Pirker <anton.pirker@sentry.io>
2024-11-08 09:24:17 +0100

    New page API Server with Developed moved underneath it.

--- src/components/sidebar/developDocsSidebar.tsx
+++ src/components/sidebar/developDocsSidebar.tsx
@@ -12,7 +12,7 @@ const devDocsMenuItems: {root: string; title: string; hideChevron?: boolean}[] =
   {root: 'getting-started', title: 'Getting Started', hideChevron: true},
   {root: 'engineering-practices', title: 'Engineering Practices'},
   {root: 'architecture', title: 'Architecture'},
-  {root: 'development', title: 'Development'},
+  {root: 'api-server', title: 'API Server'},
   {root: 'frontend', title: 'Frontend'},
   {root: 'backend', title: 'Backend'},
   {root: 'sdk', title: 'SDK Development'},
