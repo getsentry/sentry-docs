aa200956c62aeefb05d6a3d31d6cfd51141566b5
Abdellah Hariti <haritiabdellah@gmail.com>
2024-11-11 12:48:46 +0100

    Revert "add content change logic to skip build on changelog"
    This reverts commit 80d903adbf6d125b6778ea70d5d59213c18694eb.

--- apps/changelog/src/server/utils.ts
+++ apps/changelog/src/server/utils.ts
@@ -1,37 +1,42 @@
-import {prismaClient} from './prisma-client';
+import {prismaClient} from '@/server/prisma-client';
 import {Changelog} from '@prisma/client';
 import {unstable_cache} from 'next/cache';
 
-const hashHex = (buffer: ArrayBuffer) => {
-  const hexCodes = Array.from(new Uint8Array(buffer)).map(value =>
-    value.toString(16).padStart(2, '0')
-  );
-  return hexCodes.join('');
-};
-
-const hashChangelogs = async (changelogs: Changelog[]) => {
-  return crypto.subtle
-    .digest('SHA-256', new TextEncoder().encode(JSON.stringify(changelogs)))
-    .then(hashHex);
-};
-export const getChangelogsUncached = async () => {
-  const changelogs = await prismaClient.changelog.findMany({
-    include: {
-      categories: true,
-    },
-    where: {
-      published: true,
-    },
-    orderBy: {
-      publishedAt: 'desc',
-    },
-  });
-  return {
-    changelogs,
-    hash: await hashChangelogs(changelogs),
-  };
-};
-
-export const getChangelogs = unstable_cache(getChangelogsUncached, ['changelogs'], {
-  tags: ['changelogs'],
-});
+export const getChangelogs = unstable_cache(
+  async () => {
+    const changelogs = await prismaClient.changelog.findMany({
+      include: {
+        categories: true,
+      },
+      where: {
+        published: true,
+      },
+      orderBy: {
+        publishedAt: 'desc',
+      },
+    });
+    const hashHex = (buffer: ArrayBuffer) => {
+      const hexCodes = Array.from(new Uint8Array(buffer)).map(value =>
+        value.toString(16).padStart(2, '0')
+      );
+      return hexCodes.join('');
+    };
+    const hashChangelogs = (changelogs: Changelog[]) =>
+      crypto.subtle
+        .digest(
+          'SHA-256',
+          new TextEncoder().encode(
+            changelogs
+              .map(changelogs => changelogs.id + ':' + changelogs.updatedAt)
+              .join(',')
+          )
+        )
+        .then(hashHex);
+    return {
+      changelogs,
+      hash: await hashChangelogs(changelogs),
+    };
+  },
+  ['changelogs'],
+  {tags: ['changelogs']}
+);
