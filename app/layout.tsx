61d43fe1d6fdbd66c9d4e8bcc2795d722ab42a73
Charly Gomez <charly.gomez@sentry.io>
2024-11-25 14:51:33 +0100

    add kapa script

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,17 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
         data-api="https://plausible.io/api/event"
         src="https://plausible.io/js/script.tagged-events.js"
       />
+      <Script
+        async
+        src="https://widget.kapa.ai/kapa-widget.bundle.js"
+        data-website-id="ec502f3e-ef02-4b03-b345-160b3e2b6a0c"
+        data-button-hide // do not render kapa ai button
+        data-modal-override-open-class="kapa-ai-class" // all elements with this class will open the kapa ai modal
+        data-project-name="Sentry"
+        data-project-color="#6A5FC1"
+        data-project-logo="https://docs.sentry.io/_next/static/media/sentry-logo-dark.fc8e1eeb.svg"
+        data-font-family="var(--font-rubik)"
+      />
     </html>
   );
 }
