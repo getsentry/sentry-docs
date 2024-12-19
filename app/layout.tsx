9f7b2b6648d068520a9aa6b94db917d8932c690b
Charly Gomez <charly.gomez@sentry.io>
2024-12-19 16:23:52 +0100

    update integration id

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
       <Script
         async
         src="https://widget.kapa.ai/kapa-widget.bundle.js"
-        data-website-id="ec502f3e-ef02-4b03-b345-160b3e2b6a0c"
+        data-website-id="cac7cc70-969e-4bc1-a968-55534a839be4"
         data-button-hide // do not render kapa ai button
         data-modal-override-open-class="kapa-ai-class" // all elements with this class will open the kapa ai modal
         data-project-name="Sentry"
