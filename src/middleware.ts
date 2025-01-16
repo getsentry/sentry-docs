e33216a396d5a738e87a2dc489de817c4f73b3d1
Charly Gomez <charly.gomez@sentry.io>
2025-01-16 15:31:51 +0100

    fix(404): Add issue details related redirects  (#12311)

--- src/middleware.ts
+++ src/middleware.ts
@@ -2260,10 +2260,6 @@ const USER_DOCS_REDIRECTS: Redirect[] = [
     to: '/product/dashboards/',
   },
   {
-    from: '/product/error-monitoring/dashboards/',
-    to: '/product/dashboards/',
-  },
-  {
     from: '/profiling/',
     to: '/product/explore/profiling/',
   },
@@ -2287,10 +2283,7 @@ const USER_DOCS_REDIRECTS: Redirect[] = [
     from: '/profiling/mobile-app-profiling/metrics/',
     to: '/product/explore/profiling/mobile-app-profiling/metrics/',
   },
-  {
-    from: '/product/error-monitoring/filtering/',
-    to: '/concepts/data-management/filtering/',
-  },
+
   {
     from: '/data-management/rollups/',
     to: '/concepts/data-management/event-grouping/',
@@ -2548,20 +2541,8 @@ const USER_DOCS_REDIRECTS: Redirect[] = [
     to: '/product/issues/ownership-rules/',
   },
   {
-    from: '/product/error-monitoring/issue-owners/',
-    to: '/product/issues/ownership-rules/',
-  },
-  {
     from: '/product/releases/suspect-commits/',
     to: '/product/issues/suspect-commits/',
-  },
-  {
-    from: '/product/error-monitoring/',
-    to: '/product/issues/',
-  },
-  {
-    from: '/product/error-monitoring/reprocessing/',
-    to: '/product/issues/reprocessing/',
   },
   {
     from: '/product/accounts/early-adopter-features/issue-archiving/',
@@ -2623,10 +2604,7 @@ const USER_DOCS_REDIRECTS: Redirect[] = [
     from: '/product/issues/performance-issues/large-http-payload/',
     to: '/product/issues/issue-details/performance-issues/large-http-payload/',
   },
-  {
-    from: '/product/error-monitoring/breadcrumbs/',
-    to: '/product/issues/issue-details/breadcrumbs/',
-  },
+
   {
     from: '/learn/breadcrumbs/',
     to: '/product/issues/issue-details/breadcrumbs/',
