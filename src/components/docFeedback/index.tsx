db3d320297b2e6a4e0833cdd23deaf64c8ee5488
Abdellah Hariti <haritiabdellah@gmail.com>
2025-01-20 18:29:57 +0100

    clearer buttons

--- src/components/docFeedback/index.tsx
+++ src/components/docFeedback/index.tsx
@@ -17,17 +17,17 @@ export function DocFeedback({pathname}: Props) {
       <span className="text-sm">Was this helpful?</span>
       <button
         onClick={() => handleFeedback(true)}
-        className="w-10 h-10 py-2 px-2 hover:bg-[var(--gray-3)] rounded-full flex items-center justify-center"
+        className="py-2 px-4 gap-4 hover:bg-[var(--gray-3)] rounded-full flex items-center justify-center"
         aria-label="Yes, this was helpful"
       >
-        👍
+        Yes 👍
       </button>
       <button
         onClick={() => handleFeedback(false)}
-        className="w-10 h-10 py-2 px-2 hover:bg-[var(--gray-3)] rounded-full flex items-center justify-center"
+        className="py-2 px-4 gap-4 hover:bg-[var(--gray-3)] rounded-full flex items-center justify-center"
         aria-label="No, this wasn't helpful"
       >
-        👎
+        No 👎
       </button>
     </div>
   );
