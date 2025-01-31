89a36af9778a0c57eb2d713e851a6d327990c9f3
Francesco Novy <francesco.novy@sentry.io>
2025-01-31 09:42:27 +0100

    move to redirects

--- redirects.js
+++ redirects.js
@@ -471,6 +471,22 @@ const userDocsRedirects = [
     destination: '/platforms/javascript/guides/:guide/configuration/integrations/plugin/',
   },
   {
+    source: '/platforms/javascript/configuration/options/',
+    destination: '/platforms/javascript/options/',
+  },
+  {
+    source: '/platforms/javascript/guides/:guide/configuration/options/',
+    destination: '/platforms/javascript/guides/:guide/options/',
+  },
+  {
+    source: '/platforms/javascript/configuration/environments/',
+    destination: '/platforms/javascript/options/#environment',
+  },
+  {
+    source: '/platforms/javascript/guides/:guide/configuration/environments/',
+    destination: '/platforms/javascript/guides/:guide/options/#environment',
+  },
+  {
     source: '/platforms/:platform/context/',
     destination: '/platforms/:platform/enriching-events/context/',
   },
