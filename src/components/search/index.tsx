6e50a7547ed5f553f60badb5110e4e9703c81bb7
abdellah hariti <haritiabdellah@gmail.com>
2025-02-04 15:40:25 +0100

    search: respect the stored platform on non platform pages (#12477)
    * search: respect the stored platform on non platform pages

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -62,17 +62,49 @@ type Props = {
   autoFocus?: boolean;
   path?: string;
   searchPlatforms?: string[];
+  showChatBot?: boolean;
+  useStoredSearchPlatforms?: boolean;
 };
 
-export function Search({path, autoFocus, searchPlatforms = []}: Props) {
+const STORAGE_KEY = 'sentry-docs-search-platforms';
+
+export function Search({
+  path,
+  autoFocus,
+  searchPlatforms = [],
+  useStoredSearchPlatforms = true,
+}: Props) {
   const ref = useRef<HTMLDivElement>(null);
   const [query, setQuery] = useState(``);
   const [results, setResults] = useState([] as Result[]);
   const [inputFocus, setInputFocus] = useState(false);
   const [showOffsiteResults, setShowOffsiteResults] = useState(false);
   const [loading, setLoading] = useState(true);
-
+  const [currentSearchPlatforms, setCurrentSearchPlatforms] = useState(searchPlatforms);
   const pathname = usePathname();
+
+  // Load stored platforms on mount
+  useEffect(() => {
+    const storedPlatforms = localStorage.getItem(STORAGE_KEY) ?? '[]';
+    if (!storedPlatforms) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(searchPlatforms));
+    } else if (
+      storedPlatforms &&
+      searchPlatforms.length === 0 &&
+      useStoredSearchPlatforms
+    ) {
+      const platforms = JSON.parse(storedPlatforms);
+      setCurrentSearchPlatforms(platforms);
+    }
+  }, [useStoredSearchPlatforms, searchPlatforms]);
+
+  // Update stored platforms when they change
+  useEffect(() => {
+    if (searchPlatforms.length > 0) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(searchPlatforms));
+      setCurrentSearchPlatforms(searchPlatforms);
+    }
+  }, [searchPlatforms]);
 
   const handleClickOutside = useCallback((ev: MouseEvent) => {
     // don't close the search results if the user is clicking the expand button
@@ -143,7 +175,7 @@ export function Search({path, autoFocus, searchPlatforms = []}: Props) {
         inputQuery,
         {
           path,
-          platforms: searchPlatforms.map(
+          platforms: currentSearchPlatforms.map(
             platform => standardSDKSlug(platform)?.slug ?? ''
           ),
           searchAllIndexes: showOffsiteResults,
@@ -163,7 +195,7 @@ export function Search({path, autoFocus, searchPlatforms = []}: Props) {
         setResults(queryResults);
       }
     },
-    [path, searchPlatforms, showOffsiteResults, loading]
+    [path, currentSearchPlatforms, showOffsiteResults, loading]
   );
 
   const totalHits = results.reduce((a, x) => a + x.hits.length, 0);
