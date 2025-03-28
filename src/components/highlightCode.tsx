a15c1ca6d37c8f38758e0dd762352f5e8a9ca57d
Francesco Novy <francesco.novy@sentry.io>
2025-03-28 11:22:23 +0100

    less languages

--- src/components/highlightCode.tsx
+++ src/components/highlightCode.tsx
@@ -3,38 +3,21 @@ import {jsx, jsxs} from 'react/jsx-runtime';
 import {toJsxRuntime} from 'hast-util-to-jsx-runtime';
 import {Nodes} from 'hastscript/lib/create-h';
 import bash from 'refractor/lang/bash.js';
-import c from 'refractor/lang/c.js';
-import cpp from 'refractor/lang/cpp.js';
-import csharp from 'refractor/lang/csharp.js';
-import go from 'refractor/lang/go.js';
-import java from 'refractor/lang/java.js';
 import json from 'refractor/lang/json.js';
-import kotlin from 'refractor/lang/kotlin.js';
-import objectivec from 'refractor/lang/objectivec.js';
-import php from 'refractor/lang/php.js';
-import python from 'refractor/lang/python.js';
-import ruby from 'refractor/lang/ruby.js';
-import rust from 'refractor/lang/rust.js';
-import swift from 'refractor/lang/swift.js';
 import typescript from 'refractor/lang/typescript.js';
 import {refractor} from 'refractor/lib/core.js';
 
 refractor.register(bash);
 refractor.register(json);
 refractor.register(typescript);
-refractor.register(python);
-refractor.register(java);
-refractor.register(c);
-refractor.register(cpp);
-refractor.register(kotlin);
-refractor.register(csharp);
-refractor.register(rust);
-refractor.register(swift);
-refractor.register(ruby);
-refractor.register(objectivec);
-refractor.register(php);
-refractor.register(go);
+
+// If a new language should be supported, add it here and register it in refractor above
+export const SUPPORTED_LANGUAGES = ['bash', 'json', 'typescript'];
 
 export function codeToJsx(code: string, lang = 'json') {
+  if (!SUPPORTED_LANGUAGES.includes(lang)) {
+    // eslint-disable-next-line no-console
+    console.error(`Unsupported language for syntax highlighting: ${lang}`);
+  }
   return toJsxRuntime(refractor.highlight(code, lang) as Nodes, {Fragment, jsx, jsxs});
 }
