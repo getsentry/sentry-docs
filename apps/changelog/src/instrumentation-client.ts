24fe1d1505837da1592ba353daa2f9350c7b9987
Luca Forstner <luca.forstner@sentry.io>
2025-03-28 12:19:34 +0100

    Update to use instrumentation-client.ts file

--- apps/changelog/src/instrumentation-client.ts
+++ apps/changelog/src/instrumentation-client.ts
@@ -1,7 +1,3 @@
-// This file configures the initialization of Sentry on the client.
-// The config you add here will be used whenever a users loads a page in their browser.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as SentryCore from '@sentry/core';
 import * as Sentry from '@sentry/nextjs';
 import * as Spotlight from '@spotlightjs/spotlight';
