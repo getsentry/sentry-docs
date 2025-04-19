686be46a393aacecb62f43cbf7ea74e45f77c7c3
Cody De Arkland <codydearkland@gmail.com>
2025-04-19 00:10:15 -0700

    Update homepage layout and search styling

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -6,7 +6,6 @@ import PlugImage from 'sentry-docs/imgs/api.png';
 import ChatBubble from 'sentry-docs/imgs/chat-bubble.png';
 import TerminalImage from 'sentry-docs/imgs/cli.png';
 import ConceptsImage from 'sentry-docs/imgs/concepts-reference.png';
-import HeroImage from 'sentry-docs/imgs/home_illustration.png';
 import OrganizationImage from 'sentry-docs/imgs/organization.png';
 import CalculatorImage from 'sentry-docs/imgs/pricing.png';
 import RocketImage from 'sentry-docs/imgs/rocket.png';
@@ -34,41 +33,32 @@ export function Home() {
               Welcome to Sentry Docs
             </h1>
             <p className="max-w-[55ch] text-[18px] mx-auto">
-              Sentry provides end-to-end distributed tracing, enabling developers to
-              identify and debug performance issues and errors across their systems and
-              services.
+              Sentry enables developers to monitor errors, collect traces and spans, and view replays across your entire application platform so you can fix issues faster, and get back to building.
             </p>
-            <div className="w-full my-4">
+            <div className="w-full md:max-w-[calc(100%-2rem)] lg:max-w-[900px] mx-auto my-4">
               <Search path="/" searchPlatforms={[]} useStoredSearchPlatforms={false} />
             </div>
-            <div className='flex flex-row gap-4 my-4'>
+            <div className='flex flex-col md:flex-row gap-4 my-4'>
               <PlatformCard
-                className=""
+                className="w-full md:w-1/3"
                 href="/product/issues"
                 image={OrganizationImage}
                 imageAlt="Organization image"
                 title="Error Monitoring"
               />
               <PlatformCard
-                className=""
+                className="w-full md:w-1/3"
                 href="/product/tracing"
                 image={OrganizationImage}
                 imageAlt="Tracing Image"
-                title="Tracing and Spans"
+                title="Tracing"
               />
               <PlatformCard
-                className=""
-                href="/product/performance"
-                image={OrganizationImage}
-                imageAlt="Traces Image"
-                title="Performance Monitoring"
-              />
-              <PlatformCard
-                className=""
+                className="w-full md:w-1/3"
                 href="/product/explore/session-replay"
                 image={OrganizationImage}
                 imageAlt="Tracing Image"
-                title="Session Replay"
+                title="Replays"
               />
             </div>
           </div>
@@ -191,11 +181,11 @@ export function Home() {
               className="inline-block ml-1 fill-[var(--foreground)]"
             >
               <path d="M13.5535 1.01557C12.5023 0.5343 11.3926 0.192868 10.2526 0C10.0966 0.278859 9.95549 0.565767 9.82978 0.859525C8.61551 0.676548 7.38069 0.676548 6.16642 0.859525C6.04065 0.565798 5.8995 0.278893 5.74358 0C4.6029 0.194497 3.49241 0.536739 2.44013 1.01809C0.351097 4.10886 -0.215208 7.12286 0.0679445 10.0941C1.29134 10.998 2.66066 11.6854 4.11639 12.1265C4.44418 11.6856 4.73423 11.2179 4.98347 10.7283C4.51008 10.5515 4.05318 10.3334 3.61805 10.0765C3.73257 9.99339 3.84457 9.90782 3.9528 9.82476C5.21892 10.4202 6.60084 10.7289 7.99999 10.7289C9.39914 10.7289 10.7811 10.4202 12.0472 9.82476C12.1567 9.91411 12.2687 9.99969 12.3819 10.0765C11.946 10.3338 11.4882 10.5524 11.014 10.7296C11.2629 11.2189 11.553 11.6863 11.8811 12.1265C13.338 11.6872 14.7084 11.0001 15.932 10.0953C16.2643 6.64968 15.3645 3.66336 13.5535 1.01557ZM5.34213 8.26679C4.55308 8.26679 3.9012 7.55073 3.9012 6.66981C3.9012 5.78889 4.53043 5.06654 5.33961 5.06654C6.1488 5.06654 6.79565 5.78889 6.7818 6.66981C6.76796 7.55073 6.14628 8.26679 5.34213 8.26679ZM10.6578 8.26679C9.86754 8.26679 9.21817 7.55073 9.21817 6.66981C9.21817 5.78889 9.8474 5.06654 10.6578 5.06654C11.4683 5.06654 12.1101 5.78889 12.0963 6.66981C12.0824 7.55073 11.462 8.26679 10.6578 8.26679Z" />
-            </svg>
+          </svg>
           </FooterLink>
         </div>
         <p className="px-3 text-sm">
-          © {new Date().getFullYear()} • Sentry is a registered trademark of Functional
+          &copy; {new Date().getFullYear()} • Sentry is a registered trademark of Functional
           Software, Inc.
         </p>
         <div className="bg-accent-purple max-w-max md:ml-auto ml-2 px-5">
