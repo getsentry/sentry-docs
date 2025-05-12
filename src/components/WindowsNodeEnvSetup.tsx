23718018dee8a95865afbc6a6fddd7fba5c7d007
RulaKhaled <rola.abuhasna@sentry.io>
2025-05-12 12:42:27 +0200

    docs(react-router): Add note for Windows users to set  env var

---
+++ src/components/WindowsNodeEnvSetup.tsx
@@ -0,0 +1,33 @@
+import { Alert } from './alert';
+import { CodeBlock } from './codeBlock';
+import { CodeTabs } from './codeTabs';
+
+export function WindowsNodeEnvSetup({ nodeOption }: { nodeOption: string }) {
+  return (
+    <Alert>
+      <p>
+        If you're on Windows, set <code>NODE_OPTIONS</code> environment variable before
+        running your app.
+      </p>
+      <CodeTabs>
+        <CodeBlock language="cmd">
+          <pre>
+            <code>set NODE_OPTIONS={nodeOption}</code>
+          </pre>
+        </CodeBlock>
+        <CodeBlock language="powershell">
+          <pre>
+            <code>$env:NODE_OPTIONS="{nodeOption}"</code>
+          </pre>
+        </CodeBlock>
+      </CodeTabs>
+      <p className="pt-3">
+        Read more about{' '}
+        <a href="https://learn.microsoft.com/en-us/windows/win32/procthread/environment-variables">
+          environment variables
+        </a>
+        .
+      </p>
+    </Alert>
+  );
+}
--- src/mdxComponents.ts
+++ src/mdxComponents.ts
@@ -1,48 +1,49 @@
-import {Alert} from './components/alert';
-import {Arcade} from './components/arcade';
-import {Break} from './components/break';
-import {Card} from './components/card';
-import {CliChecksumTable} from './components/cliChecksumTable';
-import {CodeBlock} from './components/codeBlock';
-import {CodeTabs} from './components/codeTabs';
-import {CommunitySupportedPlatforms} from './components/communitySupportedPlatforms';
-import {ConfigKey} from './components/configKey';
-import {ConfigValue} from './components/configValue';
-import {CreateGitHubAppForm} from './components/createGitHubAppForm';
-import {DefinitionList} from './components/definitionList';
-import {DevDocsCardGrid} from './components/devDocsCardGrid';
+import { Alert } from './components/alert';
+import { Arcade } from './components/arcade';
+import { Break } from './components/break';
+import { Card } from './components/card';
+import { CliChecksumTable } from './components/cliChecksumTable';
+import { CodeBlock } from './components/codeBlock';
+import { CodeTabs } from './components/codeTabs';
+import { CommunitySupportedPlatforms } from './components/communitySupportedPlatforms';
+import { ConfigKey } from './components/configKey';
+import { ConfigValue } from './components/configValue';
+import { CreateGitHubAppForm } from './components/createGitHubAppForm';
+import { DefinitionList } from './components/definitionList';
+import { DevDocsCardGrid } from './components/devDocsCardGrid';
 import DocImage from './components/docImage';
-import {Expandable} from './components/expandable';
-import {GuideGrid} from './components/guideGrid';
-import {JsBundleList} from './components/jsBundleList';
-import {LambdaLayerDetail} from './components/lambdaLayerDetail';
-import {LinkWithPlatformIcon} from './components/linkWithPlatformIcon';
-import {OnboardingOption, OnboardingOptionButtons} from './components/onboarding';
-import {OrgAuthTokenNote} from './components/orgAuthTokenNote';
-import {PageGrid} from './components/pageGrid';
-import {ParamTable} from './components/paramTable';
-import {PiiFields} from './components/piiFields';
-import {PlatformCategorySection} from './components/platformCategorySection';
-import {PlatformFilter} from './components/platformFilter';
-import {PlatformGrid} from './components/platformGrid';
-import {PlatformIdentifier} from './components/platformIdentifier';
-import {PlatformLink} from './components/platformLink';
-import {PlatformLinkWithLogo} from './components/platformLinkWithLogo';
-import {PlatformOrGuideName} from './components/platformOrGuideName';
-import {PlatformSdkPackageName} from './components/platformSdkPackageName';
-import {PlatformSection} from './components/platformSection';
-import {RelayMetrics} from './components/relayMetrics';
-import {SandboxLink} from './components/sandboxLink';
-import {SdkApi} from './components/sdkApi';
-import {SdkOption} from './components/sdkOption';
-import {SignInNote} from './components/signInNote';
-import {SmartLink} from './components/smartLink';
-import {TableOfContents} from './components/tableOfContents';
-import {VimeoEmbed} from './components/video';
+import { Expandable } from './components/expandable';
+import { GuideGrid } from './components/guideGrid';
+import { JsBundleList } from './components/jsBundleList';
+import { LambdaLayerDetail } from './components/lambdaLayerDetail';
+import { LinkWithPlatformIcon } from './components/linkWithPlatformIcon';
+import { OnboardingOption, OnboardingOptionButtons } from './components/onboarding';
+import { OrgAuthTokenNote } from './components/orgAuthTokenNote';
+import { PageGrid } from './components/pageGrid';
+import { ParamTable } from './components/paramTable';
+import { PiiFields } from './components/piiFields';
+import { PlatformCategorySection } from './components/platformCategorySection';
+import { PlatformFilter } from './components/platformFilter';
+import { PlatformGrid } from './components/platformGrid';
+import { PlatformIdentifier } from './components/platformIdentifier';
+import { PlatformLink } from './components/platformLink';
+import { PlatformLinkWithLogo } from './components/platformLinkWithLogo';
+import { PlatformOrGuideName } from './components/platformOrGuideName';
+import { PlatformSdkPackageName } from './components/platformSdkPackageName';
+import { PlatformSection } from './components/platformSection';
+import { RelayMetrics } from './components/relayMetrics';
+import { SandboxLink } from './components/sandboxLink';
+import { SdkApi } from './components/sdkApi';
+import { SdkOption } from './components/sdkOption';
+import { SignInNote } from './components/signInNote';
+import { SmartLink } from './components/smartLink';
+import { TableOfContents } from './components/tableOfContents';
+import { VimeoEmbed } from './components/video';
+import { WindowsNodeEnvSetup } from './components/WindowsNodeEnvSetup';
 
 export function mdxComponents(
   dynamicComponents: any = {},
-  wrapper: any = ({children}) => children
+  wrapper: any = ({ children }) => children
 ) {
   return {
     Alert,
@@ -86,6 +87,7 @@ export function mdxComponents(
     SandboxLink,
     SignInNote,
     VimeoEmbed,
+    WindowsNodeEnvSetup,
     a: SmartLink,
     img: DocImage,
     ...dynamicComponents,
