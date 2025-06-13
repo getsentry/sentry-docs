5e6575aefb09a5a2acb3e5f48baa325ed0849a08
Anton Pirker <anton.pirker@sentry.io>
2025-06-13 14:44:49 +0200

    descriptions

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,37 +1,37 @@
 import * as Sentry from '@sentry/nextjs';
 
-export function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      tracesSampleRate: 1,
-      debug: false,
-      environment: process.env.NODE_ENV === 'development' ? 'development' : undefined,
-      spotlight: process.env.NODE_ENV === 'development',
-    });
-  }
+// export function register() {
+//   if (process.env.NEXT_RUNTIME === 'nodejs') {
+//     Sentry.init({
+//       dsn: process.env.SENTRY_DSN,
+//       tracesSampleRate: 1,
+//       debug: false,
+//       environment: process.env.NODE_ENV === 'development' ? 'development' : undefined,
+//       spotlight: process.env.NODE_ENV === 'development',
+//     });
+//   }
 
-  if (process.env.NEXT_RUNTIME === 'edge') {
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      tracesSampleRate: 1,
-      debug: false,
-      environment: process.env.NODE_ENV === 'development' ? 'development' : undefined,
-      // temporary change for investigating edge middleware tx names
-      beforeSendTransaction(event) {
-        if (
-          event.transaction?.includes('middleware GET') &&
-          event.contexts?.trace?.data
-        ) {
-          event.contexts.trace.data = {
-            ...event.contexts.trace.data,
-            'sentry.source': 'custom',
-          };
-        }
-        return event;
-      },
-    });
-  }
-}
+//   if (process.env.NEXT_RUNTIME === 'edge') {
+//     Sentry.init({
+//       dsn: process.env.SENTRY_DSN,
+//       tracesSampleRate: 1,
+//       debug: false,
+//       environment: process.env.NODE_ENV === 'development' ? 'development' : undefined,
+//       // temporary change for investigating edge middleware tx names
+//       beforeSendTransaction(event) {
+//         if (
+//           event.transaction?.includes('middleware GET') &&
+//           event.contexts?.trace?.data
+//         ) {
+//           event.contexts.trace.data = {
+//             ...event.contexts.trace.data,
+//             'sentry.source': 'custom',
+//           };
+//         }
+//         return event;
+//       },
+//     });
+//   }
+// }
 
-export const onRequestError = Sentry.captureRequestError;
+// export const onRequestError = Sentry.captureRequestError;
