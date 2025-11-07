2bfe39a79e22bd50ad80e9be7d2a2ce05cdfcabf
Charly Gomez <charly.gomez@sentry.io>
2025-11-07 22:32:38 +0100

    docs(js): Add metrics page (#15353)
    Co-authored-by: Jan Peer Stöcklmair <jan.peer@sentry.io>
    Co-authored-by: Alex Krawiec <alex.krawiec@sentry.io>

--- redirects.js
+++ redirects.js
@@ -1081,14 +1081,6 @@ const userDocsRedirects = [
     destination: '/platforms/javascript/tracing/distributed-tracing/:path*',
   },
   {
-    source: '/platforms/javascript/metrics/:path*',
-    destination: '/platforms/javascript/tracing/span-metrics/:path*',
-  },
-  {
-    source: '/platforms/javascript/guides/:guide/metrics/:path*',
-    destination: '/platforms/javascript/guides/:guide/tracing/span-metrics/:path*',
-  },
-  {
     source: '/platforms/javascript/tracing/instrumentation/performance-metrics/',
     destination: '/platforms/javascript/tracing/span-metrics/',
   },
