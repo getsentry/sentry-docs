cf26d100d189413054396325333c5402fefc90ac
Shannon Anahata <shannonanahata@gmail.com>
2025-11-20 15:38:25 -0800

    Fix hover prefetch to skip metrics tracking - avoid inflating search analytics

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -401,7 +401,7 @@ export function Search({
               <button
                 className={styles['sgs-expand-results-button']}
                 onClick={() => setShowOffsiteResults(true)}
-                onMouseOver={() => searchFor(query, {searchAllIndexes: true})}
+                onMouseOver={() => searchFor(query, {searchAllIndexes: true, skipMetrics: true})}
               >
                 Search <em>{query}</em> across all Sentry sites
               </button>
