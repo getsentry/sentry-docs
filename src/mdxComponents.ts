60c0d3207228d411d954485b3a59eadf138d86c1
Shannon Anahata <shannonanahata@gmail.com>
2026-02-09 15:34:00 -0800

    fixing bugs

--- src/mdxComponents.ts
+++ src/mdxComponents.ts
@@ -23,6 +23,7 @@ import {GradleUploadInstructions} from './components/gradleUploadInstructions';
 import {GuideGrid} from './components/guideGrid';
 import {JsBundleList} from './components/jsBundleList';
 import {LambdaLayerDetail} from './components/lambdaLayerDetail';
+import {LinkCard, LinkCardGrid} from './components/linkCard';
 import {LinkWithPlatformIcon} from './components/linkWithPlatformIcon';
 import {LLMSteering} from './components/llmSteering';
 import {
@@ -125,6 +126,8 @@ export function mdxComponents(
     a: SmartLink,
     img: DocImage,
     ...dynamicComponents,
+    LinkCard,
+    LinkCardGrid,
     wrapper,
   };
 }
