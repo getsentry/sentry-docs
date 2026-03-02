86207cc1301841b13f5ed9f014ab17c72248f29d
Charly Gomez <charly.gomez@sentry.io>
2026-03-02 14:24:56 +0100

    switch

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,6 +18,7 @@ export const config = {
     // - favicon.ico (favicon file)
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
+  runtime: 'nodejs',
 };
 
 // This function can be marked `async` if using `await` inside
