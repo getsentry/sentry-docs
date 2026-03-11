dbf88b893e90b736ebcfd9939b2330028135b60f
Shannon Anahata <shannonanahata@gmail.com>
2026-03-11 15:39:03 -0700

    Fixed some small bugs in a redirect and typos.

--- redirects.js
+++ redirects.js
@@ -1343,14 +1343,10 @@ const userDocsRedirects = [
     destination: '/organization/dynamic-sampling/',
   },
   {
-    source: '/product/insights/overview/metrics/',
-    destination: '/product/dashboards/sentry-dashboards/transaction-summary/',
-  },
-  {
     source: '/product/dashboards/sentry-dashboards/performance-metrics/',
     destination: '/product/dashboards/sentry-dashboards/transaction-summary/',
   },
-  // End of Insights reduirects.
+  // End of Insights redirects.
   {
     source: '/platforms/javascript/guides/astro/manual-setup/',
     destination: '/platforms/javascript/guides/astro/',
