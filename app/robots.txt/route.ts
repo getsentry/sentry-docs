d4094fb4952b3e20a482df791b4a24a8b882ebcd
Alex Krawiec <alex.krawiec@sentry.io>
2026-05-18 11:10:56 -0700

    Add user agent rules for ai crawlers in robots.txt

--- app/robots.txt/route.ts
+++ app/robots.txt/route.ts
@@ -1,10 +1,43 @@
 import {isDeveloperDocs} from 'sentry-docs/isDeveloperDocs';
 
+const sitemap = isDeveloperDocs
+  ? 'https://develop.sentry.dev/sitemap.xml'
+  : 'https://docs.sentry.io/sitemap.xml';
+
 export const GET = (_request: Request) => {
   return new Response(
     `
-Sitemap: ${isDeveloperDocs ? 'https://develop.sentry.dev/sitemap.xml' : 'https://docs.sentry.io/sitemap.xml'}
+Sitemap: ${sitemap}
+
 User-agent: *
+Allow: /
+
+User-agent: GPTBot
+Allow: /
+
+User-agent: OAI-SearchBot
+Allow: /
+
+User-agent: Claude-Web
+Allow: /
+
+User-agent: anthropic-ai
+Allow: /
+
+User-agent: Google-Extended
+Allow: /
+
+User-agent: Amazonbot
+Allow: /
+
+User-agent: Bytespider
+Allow: /
+
+User-agent: CCBot
+Allow: /
+
+User-agent: Applebot-Extended
+Allow: /
 `.trim(),
     {headers: {'content-type': 'text/plain'}}
   );
